1d72927ad97effd8be23aa4fa2ce01c8b4a36578
Gonzalo Reyero Ferreras <87083379+greyerof@users.noreply.github.com>
2022-03-02 15:02:39 +0100

    Observability TS - TC migration: observability-container-logging (#40)
    Observability TS - TC migration: observability-container-logging

--- cnf-certification-test/observability/suite.go
+++ cnf-certification-test/observability/suite.go
@@ -17,14 +17,86 @@
 package observability
 
 import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+
 	"github.com/onsi/ginkgo/v2"
 	"github.com/sirupsen/logrus"
 	"github.com/test-network-function/cnf-certification-test/cnf-certification-test/common"
+	"github.com/test-network-function/cnf-certification-test/cnf-certification-test/identifiers"
+	"github.com/test-network-function/cnf-certification-test/internal/clientsholder"
+	"github.com/test-network-function/cnf-certification-test/pkg/provider"
+	"github.com/test-network-function/cnf-certification-test/pkg/tnf"
+	v1 "k8s.io/api/core/v1"
 )
 
 //
 // All actual test code belongs below here.  Utilities belong above.
 //
 var _ = ginkgo.Describe(common.ObservabilityTestKey, func() {
-	logrus.Debugf("%s not moved yet to new framework", common.ObservabilityTestKey)
+	var env provider.TestEnvironment
+	ginkgo.BeforeEach(func() {
+		provider.BuildTestEnvironment()
+		env = provider.GetTestEnvironment()
+	})
+
+	testID := identifiers.XformToGinkgoItIdentifier(identifiers.TestLoggingIdentifier)
+	ginkgo.It(testID, ginkgo.Label(testID), func() {
+		testContainersLogging(&env)
+	})
 })
+
+// containerHasLoggingOutput helper function to get the last line of logging output from
+// a container. Returns true in case some output was found, false otherwise.
+func containerHasLoggingOutput(cut *provider.Container) (bool, error) {
+	ocpClient := clientsholder.NewClientsHolder()
+
+	numLogLines := int64(1)
+	podLogOptions := v1.PodLogOptions{TailLines: &numLogLines, Container: cut.Data.Name}
+	req := ocpClient.Coreclient.Pods(cut.Namespace).GetLogs(cut.Podname, &podLogOptions)
+
+	podLogsReaderCloser, err := req.Stream(context.TODO())
+	if err != nil {
+		return false, fmt.Errorf("unable to get log streamer, err: %s", err)
+	}
+
+	defer podLogsReaderCloser.Close()
+
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, podLogsReaderCloser)
+	if err != nil {
+		return false, fmt.Errorf("unable to get log data, err: %s", err)
+	}
+
+	return buf.String() != "", nil
+}
+
+func testContainersLogging(env *provider.TestEnvironment) {
+	if len(env.Containers) == 0 {
+		ginkgo.Skip("No containers to run test, skipping")
+	}
+
+	// Iterate through all the CUTs to get their log output. The TC checks that at least
+	// one log line is found.
+	badContainers := []string{}
+	for _, cut := range env.Containers {
+		ginkgo.By(fmt.Sprintf("Checking container %s has some logging output", cut.StringShort()))
+		hasLoggingOutput, err := containerHasLoggingOutput(cut)
+		if err != nil {
+			tnf.ClaimFilePrintf("Failed to get container %s log output: %s", cut.StringShort(), err)
+			badContainers = append(badContainers, cut.StringShort())
+		}
+
+		if !hasLoggingOutput {
+			tnf.ClaimFilePrintf("Container: %s does not have any line of log to stderr/stdout", cut.StringShort())
+			badContainers = append(badContainers, cut.StringShort())
+		}
+	}
+
+	if n := len(badContainers); n > 0 {
+		logrus.Debugf("Containers without logging: %+v", badContainers)
+		ginkgo.Fail(fmt.Sprintf("%d containers don't have any log to stdout/stderr.", n))
+	}
+}
