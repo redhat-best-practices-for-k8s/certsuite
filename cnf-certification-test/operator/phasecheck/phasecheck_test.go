5ec95736e2615d9f77aea9f57d4b8c5e07a0186c
David Elie-Dit-Cosaque <deliedit@redhat.com>
2022-03-15 16:21:55 -0500

    adding unit test

---
+++ cnf-certification-test/operator/phasecheck/phasecheck_test.go
@@ -0,0 +1,165 @@
+// Copyright (C) 2020-2022 Red Hat, Inc.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+package phasecheck
+
+import (
+	"testing"
+
+	"github.com/operator-framework/api/pkg/operators/v1alpha1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func Test_isOperatorSucceeded(t *testing.T) { //nolint:funlen
+	type args struct {
+		csv *v1alpha1.ClusterServiceVersion
+	}
+	tests := []struct {
+		name        string
+		args        args
+		wantIsReady bool
+	}{
+		{name: "ok",
+			args: args{csv: &v1alpha1.ClusterServiceVersion{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "aCSV",
+					Namespace: "aNamespace",
+				},
+				Status: v1alpha1.ClusterServiceVersionStatus{
+					Phase: v1alpha1.CSVPhaseSucceeded,
+				},
+			}},
+			wantIsReady: true,
+		},
+		{name: "nok",
+			args: args{csv: &v1alpha1.ClusterServiceVersion{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "aCSV",
+					Namespace: "aNamespace",
+				},
+				Status: v1alpha1.ClusterServiceVersionStatus{
+					Phase: v1alpha1.CSVPhaseInstalling,
+				},
+			}},
+			wantIsReady: false,
+		},
+		{name: "nok",
+			args: args{csv: &v1alpha1.ClusterServiceVersion{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "aCSV",
+					Namespace: "aNamespace",
+				},
+				Status: v1alpha1.ClusterServiceVersionStatus{
+					Phase: v1alpha1.CSVPhaseDeleting,
+				},
+			}},
+			wantIsReady: false,
+		},
+		{name: "nok",
+			args: args{csv: &v1alpha1.ClusterServiceVersion{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "aCSV",
+					Namespace: "aNamespace",
+				},
+				Status: v1alpha1.ClusterServiceVersionStatus{
+					Phase: v1alpha1.CSVPhaseInstallReady,
+				},
+			}},
+		},
+		{name: "nok",
+			args: args{csv: &v1alpha1.ClusterServiceVersion{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "aCSV",
+					Namespace: "aNamespace",
+				},
+				Status: v1alpha1.ClusterServiceVersionStatus{
+					Phase: v1alpha1.CSVPhaseUnknown,
+				},
+			}},
+			wantIsReady: false,
+		},
+		{name: "nok",
+			args: args{csv: &v1alpha1.ClusterServiceVersion{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "aCSV",
+					Namespace: "aNamespace",
+				},
+				Status: v1alpha1.ClusterServiceVersionStatus{
+					Phase: v1alpha1.CSVPhasePending,
+				},
+			}},
+			wantIsReady: false,
+		},
+		{name: "nok",
+			args: args{csv: &v1alpha1.ClusterServiceVersion{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "aCSV",
+					Namespace: "aNamespace",
+				},
+				Status: v1alpha1.ClusterServiceVersionStatus{
+					Phase: v1alpha1.CSVPhaseReplacing,
+				},
+			}},
+			wantIsReady: false,
+		},
+		{name: "nok",
+			args: args{csv: &v1alpha1.ClusterServiceVersion{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "aCSV",
+					Namespace: "aNamespace",
+				},
+				Status: v1alpha1.ClusterServiceVersionStatus{
+					Phase: v1alpha1.CSVPhaseAny,
+				},
+			}},
+			wantIsReady: false,
+		},
+		{name: "nok",
+			args: args{csv: &v1alpha1.ClusterServiceVersion{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "aCSV",
+					Namespace: "aNamespace",
+				},
+				Status: v1alpha1.ClusterServiceVersionStatus{
+					Phase: v1alpha1.CSVPhaseFailed,
+				},
+			}},
+
+			wantIsReady: false,
+		},
+		{name: "nok",
+			args: args{csv: &v1alpha1.ClusterServiceVersion{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "aCSV",
+					Namespace: "aNamespace",
+				},
+				Status: v1alpha1.ClusterServiceVersionStatus{
+					Phase: v1alpha1.CSVPhaseNone,
+				},
+			}},
+
+			wantIsReady: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotIsReady := isOperatorSucceeded(tt.args.csv); gotIsReady != tt.wantIsReady {
+				t.Errorf("isOperatorSucceeded() = %v, want %v", gotIsReady, tt.wantIsReady)
+			}
+		})
+	}
+}
