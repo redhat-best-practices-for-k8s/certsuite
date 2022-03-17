4ee825918b8e531ec067ce15c50b7320a8245ba3
aabughosh <aabugosh@redhat.com>
2022-03-17 19:12:30 +0200

    update the nolint:dupl in statfefulset and deployment files

--- cnf-certification-test/lifecycle/suite.go
+++ cnf-certification-test/lifecycle/suite.go
@@ -313,7 +313,6 @@ func testStatefulSetScaling(env *provider.TestEnvironment, timeout time.Duration
 	}
 	gomega.Expect(0).To(gomega.Equal(len(failedSatetfulSets)))
 	gomega.Expect(0).To(gomega.Equal(len(skippedSatetfulSets)))
-
 }
 
 // testHighAvailability
