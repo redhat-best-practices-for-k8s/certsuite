4de9cc9ba2155f6c70d3e4bd313c852c4ee2edea
Brandon Palm <bpalm@redhat.com>
2022-03-22 17:01:26 -0500

    Clean up automount tests with helper funcs and more unit tests

--- cnf-certification-test/accesscontrol/rbac/automount.go
+++ cnf-certification-test/accesscontrol/rbac/automount.go
@@ -18,30 +18,75 @@ package rbac
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/sirupsen/logrus"
 	"github.com/test-network-function/cnf-certification-test/internal/clientsholder"
+	v1core "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type AutomountTokenFuncs interface {
 	AutomountServiceAccountSetOnSA(serviceAccountName, podNamespace string) (*bool, error)
+	EvaluateTokens(put *v1core.Pod) (bool, string)
 }
 type AutomountToken struct {
-	ch *clientsholder.ClientsHolder
+	clientsHolder *clientsholder.ClientsHolder
 }
 
 func NewAutomountTokenTester(ch *clientsholder.ClientsHolder) *AutomountToken {
 	return &AutomountToken{
-		ch: ch,
+		clientsHolder: ch,
 	}
 }
 
 func (at *AutomountToken) AutomountServiceAccountSetOnSA(serviceAccountName, podNamespace string) (*bool, error) {
-	sa, err := at.ch.K8sClient.CoreV1().ServiceAccounts(podNamespace).Get(context.TODO(), serviceAccountName, v1.GetOptions{})
+	sa, err := at.clientsHolder.K8sClient.CoreV1().ServiceAccounts(podNamespace).Get(context.TODO(), serviceAccountName, v1.GetOptions{})
 	if err != nil {
 		logrus.Errorf("executing serviceaccount command failed with error: %s", err)
 		return nil, err
 	}
 	return sa.AutomountServiceAccountToken, nil
+}
+
+//nolint:gocritic
+func (at *AutomountToken) EvaluateTokens(put *v1core.Pod) (bool, string) {
+	// The token can be specified in the pod directly
+	// or it can be specified in the service account of the pod
+	// if no service account is configured, then the pod will use the configuration
+	// of the default service account in that namespace
+	// the token defined in the pod has takes precedence
+	// the test would pass iif token is explicitly set to false
+	// if the token is set to true in the pod, the test would fail right away
+	if put.Spec.AutomountServiceAccountToken != nil && *put.Spec.AutomountServiceAccountToken {
+		return false, fmt.Sprintf("Pod %s:%s is configured with automountServiceAccountToken set to true", put.Namespace, put.Name)
+	}
+
+	// Collect information about the service account attached to the pod.
+	saAutomountServiceAccountToken, err := at.AutomountServiceAccountSetOnSA(put.Spec.ServiceAccountName, put.Namespace)
+	if err != nil {
+		return false, ""
+	}
+
+	// The pod token is false means the pod is configured properly
+	// The pod is not configured and the service account is configured with false means
+	// the pod will inherit the behavior `false` and the test would pass
+	if (put.Spec.AutomountServiceAccountToken != nil && !*put.Spec.AutomountServiceAccountToken) || (saAutomountServiceAccountToken != nil && !*saAutomountServiceAccountToken) {
+		return true, ""
+	}
+
+	// the service account is configured with true means all the pods
+	// using this service account are not configured properly, register the error
+	// message and fail
+	if saAutomountServiceAccountToken != nil && *saAutomountServiceAccountToken {
+		return false, fmt.Sprintf("serviceaccount %s:%s is configured with automountServiceAccountToken set to true, impacting pod %s", put.Namespace, put.Spec.ServiceAccountName, put.Name)
+	}
+
+	// the token should be set explicitly to false, otherwise, it's a failure
+	// register the error message and check the next pod
+	if saAutomountServiceAccountToken == nil {
+		return false, fmt.Sprintf("serviceaccount %s:%s is not configured with automountServiceAccountToken set to false, impacting pod %s", put.Namespace, put.Spec.ServiceAccountName, put.Name)
+	}
+
+	return true, "" // Pod has passed all checks
 }
--- cnf-certification-test/accesscontrol/suite.go
+++ cnf-certification-test/accesscontrol/suite.go
@@ -378,7 +378,6 @@ func TestPodClusterRoleBindings(env *provider.TestEnvironment) {
 	}
 }
 
-//nolint:funlen
 func TestAutomountServiceToken(env *provider.TestEnvironment, testerFuncs rbac.AutomountTokenFuncs) {
 	ginkgo.By("Should have automountServiceAccountToken set to false")
 
@@ -388,46 +387,11 @@ func TestAutomountServiceToken(env *provider.TestEnvironment, testerFuncs rbac.A
 		ginkgo.By(fmt.Sprintf("check the existence of pod service account %s (ns= %s )", put.Namespace, put.Name))
 		gomega.Expect(put.Spec.ServiceAccountName).ToNot(gomega.BeEmpty())
 
-		// The token can be specified in the pod directly
-		// or it can be specified in the service account of the pod
-		// if no service account is configured, then the pod will use the configuration
-		// of the default service account in that namespace
-		// the token defined in the pod has takes precedence
-		// the test would pass iif token is explicitly set to false
-		// if the token is set to true in the pod, the test would fail right away
-		if put.Spec.AutomountServiceAccountToken != nil && *put.Spec.AutomountServiceAccountToken {
-			msg = append(msg, fmt.Sprintf("Pod %s:%s is configured with automountServiceAccountToken set to true ", put.Namespace, put.Name))
+		// Evaluate the pod's automount service tokens and any attached service accounts
+		podPassed, newMsg := testerFuncs.EvaluateTokens(put)
+		if !podPassed {
 			failedPods = append(failedPods, put.Name)
-			continue
-		}
-
-		// Collect information about the service account attached to the pod.
-		saAutomountServiceAccountToken, err := testerFuncs.AutomountServiceAccountSetOnSA(put.Spec.ServiceAccountName, put.Namespace)
-		if err != nil {
-			failedPods = append(failedPods, put.Name)
-			continue
-		}
-
-		// The pod token is false means the pod is configured properly
-		// The pod is not configured and the service account is configured with false means
-		// the pod will inherit the behavior `false` and the test would pass
-		if (put.Spec.AutomountServiceAccountToken != nil && !*put.Spec.AutomountServiceAccountToken) || (saAutomountServiceAccountToken != nil && !*saAutomountServiceAccountToken) {
-			continue
-		}
-
-		// the service account is configured with true means all the pods
-		// using this service account are not configured properly, register the error
-		// message and fail
-		if saAutomountServiceAccountToken != nil && *saAutomountServiceAccountToken {
-			msg = append(msg, fmt.Sprintf("serviceaccount %s:%s is configured with automountServiceAccountToken set to true, impacting pod %s ", put.Namespace, put.Spec.ServiceAccountName, put.Name))
-			failedPods = append(failedPods, put.Name)
-		}
-
-		// the token should be set explicitly to false, otherwise, it's a failure
-		// register the error message and check the next pod
-		if saAutomountServiceAccountToken == nil {
-			msg = append(msg, fmt.Sprintf("serviceaccount %s:%s is not configured with automountServiceAccountToken set to false, impacting pod %s ", put.Namespace, put.Spec.ServiceAccountName, put.Name))
-			failedPods = append(failedPods, put.Name)
+			msg = append(msg, newMsg)
 		}
 	}
 
