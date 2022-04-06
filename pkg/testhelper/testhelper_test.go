ce9eec0499fa1dc88d712ab22d20bbd274437a13
Brandon Palm <bpalm@redhat.com>
2022-04-06 10:43:15 -0500

    Add tests for 'testhelper' package

--- pkg/testhelper/testhelper_test.go
+++ pkg/testhelper/testhelper_test.go
@@ -15,3 +15,25 @@
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 package testhelper
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestResultToString(t *testing.T) {
+	testCases := []struct {
+		input          int
+		expectedResult string
+	}{
+		{input: SUCCESS, expectedResult: "SUCCESS"},
+		{input: FAILURE, expectedResult: "FAILURE"},
+		{input: ERROR, expectedResult: "ERROR"},
+		{input: 1337, expectedResult: ""},
+	}
+
+	for _, tc := range testCases {
+		assert.Equal(t, tc.expectedResult, ResultToString(tc.input))
+	}
+}
