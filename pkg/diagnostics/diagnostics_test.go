2e0bd600fb58dfdcda2c0010e5e7c8b313111b67
Brandon Palm <bpalm@redhat.com>
2022-04-13 10:14:03 -0500

    Add unit test for diagnostics package (#122)
    * Add unit test for diagnostics package
    
    * Address comments; Add getHWTextOutput func

--- pkg/diagnostics/diagnostics_test.go
+++ pkg/diagnostics/diagnostics_test.go
@@ -16,3 +16,145 @@
 
 // Package diagnostic provides a test suite which gathers OpenShift cluster information.
 package diagnostics
+
+import (
+	"encoding/json"
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/test-network-function/cnf-certification-test/internal/clientsholder"
+	v1 "k8s.io/api/core/v1"
+)
+
+func TestGetVersionOcClient(t *testing.T) {
+	assert.Equal(t, "n/a, (not using oc or kubectl client)", GetVersionOcClient())
+}
+
+//nolint:funlen
+func TestGetHWJsonOutput(t *testing.T) {
+	type TestingJSON struct {
+		Testing string `json:"testing"`
+	}
+
+	testCases := []struct {
+		execStdout string
+		execStderr string
+		execErr    error
+
+		expectedErr    error
+		expectedResult TestingJSON
+	}{
+		{
+			execStdout: `{"testing":"hello world"}`,
+			execStderr: "",
+			execErr:    nil,
+
+			expectedErr: nil,
+			expectedResult: TestingJSON{
+				Testing: "hello world",
+			},
+		},
+		{
+			execStdout: `{"testing":"hello world"}`,
+			execStderr: "this is an error",
+			execErr:    nil,
+
+			expectedErr:    errors.New("command does not matter failed with error err: %!s(<nil>) , stderr: this is an error"),
+			expectedResult: TestingJSON{},
+		},
+		{
+			execStdout: `{"testing":"hello world"}`,
+			execStderr: "this is an error",
+			execErr:    errors.New("this is an error2"),
+
+			expectedErr:    errors.New("command does not matter failed with error err: %!s(<nil>) , stderr: this is an error"),
+			expectedResult: TestingJSON{},
+		},
+	}
+
+	for _, tc := range testCases {
+		result, err := getHWJsonOutput(&v1.Pod{
+			Spec: v1.PodSpec{
+				// Note: We don't actually care about the podname
+				// for this test, but the function uses it to build the
+				// context .
+				Containers: []v1.Container{
+					{
+						Name: "podname",
+					},
+				},
+			},
+		}, &clientsholder.CommandMock{
+			ExecCommandContainerFunc: func(context clientsholder.Context, s string) (string, string, error) {
+				return tc.execStdout, tc.execStderr, nil
+			},
+		}, "does not matter")
+
+		tj := TestingJSON{}
+		tjBytes, _ := json.Marshal(result)
+		_ = json.Unmarshal(tjBytes, &tj)
+
+		assert.Equal(t, tc.expectedErr, err)
+		assert.Equal(t, tc.expectedResult, tj)
+	}
+}
+
+//nolint:funlen
+func TestGetHWTextOutput(t *testing.T) {
+	testCases := []struct {
+		execStdout string
+		execStderr string
+		execErr    error
+
+		expectedErr    error
+		expectedResult []string
+	}{
+		{
+			execStdout: "hello\nworld",
+			execStderr: "",
+			execErr:    nil,
+
+			expectedErr:    nil,
+			expectedResult: []string{"hello", "world"},
+		},
+		{
+			execStdout: `{"testing":"hello world"}`,
+			execStderr: "this is an error",
+			execErr:    nil,
+
+			expectedErr:    errors.New("command lspci failed with error err: %!s(<nil>) , stderr: this is an error"),
+			expectedResult: nil,
+		},
+		{
+			execStdout: `{"testing":"hello world"}`,
+			execStderr: "this is an error",
+			execErr:    errors.New("this is an error2"),
+
+			expectedErr:    errors.New("command lspci failed with error err: %!s(<nil>) , stderr: this is an error"),
+			expectedResult: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		result, err := getHWTextOutput(&v1.Pod{
+			Spec: v1.PodSpec{
+				// Note: We don't actually care about the podname
+				// for this test, but the function uses it to build the
+				// context .
+				Containers: []v1.Container{
+					{
+						Name: "podname",
+					},
+				},
+			},
+		}, &clientsholder.CommandMock{
+			ExecCommandContainerFunc: func(context clientsholder.Context, s string) (string, string, error) {
+				return tc.execStdout, tc.execStderr, nil
+			},
+		}, lspciCommand)
+
+		assert.Equal(t, tc.expectedErr, err)
+		assert.Equal(t, tc.expectedResult, result)
+	}
+}
