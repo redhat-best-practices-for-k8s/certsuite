3418408c3c18a799bccb6ecb5a9a7169c51a4306
David Elie-Dit-Cosaque <deliedit@redhat.com>
2022-04-25 16:54:59 -0500

    Adding Boot params test case

---
+++ pkg/arrayhelper/arrayhelper.go
@@ -0,0 +1,47 @@
+// Copyright (C) 2020-2021 Red Hat, Inc.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+package arrayhelper
+
+import (
+	"strings"
+)
+
+// ArgListToMap takes a list of strings of the form "key=value" and translate it into a map
+// of the form {key: value}
+func ArgListToMap(lst []string) map[string]string {
+	retval := make(map[string]string)
+	for _, arg := range lst {
+		splitArgs := strings.Split(arg, "=")
+		if len(splitArgs) == 1 {
+			retval[splitArgs[0]] = ""
+		} else {
+			retval[splitArgs[0]] = splitArgs[1]
+		}
+	}
+	return retval
+}
+
+// FilterArray takes a list and a predicate and returns a list of all elements for whom the predicate returns true
+func FilterArray(vs []string, f func(string) bool) []string {
+	vsf := make([]string, 0)
+	for _, v := range vs {
+		if f(v) {
+			vsf = append(vsf, v)
+		}
+	}
+	return vsf
+}
