6d6ad5d6b84cb2e049e5c89d568c5542ba452281
Salaheddine Hamadi <shamadi@redhat.com>
2022-05-03 13:47:58 -0400

    address bug in certification implementation

--- internal/registry/operator_test.go
+++ internal/registry/operator_test.go
@@ -1,7 +1,21 @@
+// Copyright (C) 2020-2022 Red Hat, Inc.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 package registry
 
 import (
-	"fmt"
 	"os"
 	"testing"
 
@@ -9,7 +23,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test(t *testing.T) {
+func TestIsOperatorCertified(t *testing.T) {
 	name := "zoperator.v0.3.6"
 	ocpversion := "4.6"
 	path, _ := os.Getwd()
@@ -20,6 +34,8 @@ func Test(t *testing.T) {
 	}
 	loadOperatorsCatalog(path + "/../")
 	ans := IsOperatorCertified(name, ocpversion)
-	fmt.Println(ans)
 	assert.Equal(t, ans, true)
+
+	ans = IsOperatorCertified("falcon-alpha", ocpversion)
+	assert.Equal(t, ans, false)
 }
