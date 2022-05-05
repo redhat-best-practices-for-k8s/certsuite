7077c89f3393a5bd964c861304a6b699bd170933
Brandon Palm <bpalm@redhat.com>
2022-05-05 14:31:30 -0500

    Add shellcheck linter

--- script/create-version-files.sh
+++ script/create-version-files.sh
@@ -5,6 +5,6 @@ GIT_COMMIT=$(git rev-list -1 HEAD)
 
 curl -s https://api.github.com/repos/test-network-function/cnf-certification-test/releases| jq -r ".[].tag_name"|sort > all-releases.txt
 git tag --points-at HEAD |sort > release-tag.txt
-git tag --no-contains ${GIT_COMMIT}|tail -n1|sort > latest-release-tag.txt
+git tag --no-contains "${GIT_COMMIT}"|tail -n1|sort > latest-release-tag.txt
 
-echo $GIT_COMMIT
+echo "$GIT_COMMIT"
