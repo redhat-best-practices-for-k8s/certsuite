b9ca5998424ef0aa3f871bc8659287644a0becf0
Brandon Palm <bpalm@redhat.com>
2022-06-21 13:34:30 -0500

    Add netcommons tests (#285)

--- cnf-certification-test/networking/netcommons/netcommons_test.go
+++ cnf-certification-test/networking/netcommons/netcommons_test.go
@@ -18,7 +18,12 @@ package netcommons
 
 import (
 	"reflect"
+	"sort"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/test-network-function/cnf-certification-test/pkg/provider"
+	corev1 "k8s.io/api/core/v1"
 )
 
 func Test_getIPVersion(t *testing.T) {
@@ -94,5 +99,181 @@ func TestFilterIPListByIPVersion(t *testing.T) {
 				t.Errorf("FilterIPListByIPVersion() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func TestPodIPsToStringList(t *testing.T) {
+	testCases := []struct {
+		testPodIPs   []corev1.PodIP
+		expectedList []string
+	}{
+		{ // Test Case #1 - different IPs
+			testPodIPs: []corev1.PodIP{
+				{
+					IP: "192.168.1.1",
+				},
+				{
+					IP: "192.168.1.2",
+				},
+			},
+			expectedList: []string{"192.168.1.1", "192.168.1.2"},
+		},
+		{ // Test Case #2 - same IPs
+			testPodIPs: []corev1.PodIP{
+				{
+					IP: "192.168.1.1",
+				},
+				{
+					IP: "192.168.1.1",
+				},
+			},
+			expectedList: []string{"192.168.1.1", "192.168.1.1"},
+		},
+		{ // Test Case #3 - no IPs
+			testPodIPs:   []corev1.PodIP{},
+			expectedList: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		results := PodIPsToStringList(tc.testPodIPs)
+		sort.Strings(results)
+		assert.Equal(t, tc.expectedList, results)
+	}
+}
+
+func TestContainerIPString(t *testing.T) {
+	generateCIP := func(IP, ID string) *ContainerIP {
+		return &ContainerIP{
+			IP: IP,
+			ContainerIdentifier: &provider.Container{
+				UID: ID,
+				Data: &corev1.Container{
+					Name: "test-" + ID,
+				},
+			},
+		}
+	}
+
+	testCases := []struct {
+		testID         string
+		testIP         string
+		expectedString string
+	}{
+		{
+			testID:         "ID1",
+			testIP:         "192.168.1.1",
+			expectedString: "192.168.1.1 ( node:  ns:  podName:  containerName: test-ID1 containerUID:  containerRuntime:  )",
+		},
+		{
+			testID:         "ID2",
+			testIP:         "192.168.1.2",
+			expectedString: "192.168.1.2 ( node:  ns:  podName:  containerName: test-ID2 containerUID:  containerRuntime:  )",
+		},
+	}
+
+	for _, tc := range testCases {
+		cip := generateCIP(tc.testIP, tc.testID)
+		assert.Equal(t, tc.expectedString, cip.String())
+	}
+}
+
+func TestNetTestContextString(t *testing.T) {
+	buildTestContext := func(IP, containerName string) NetTestContext {
+		return NetTestContext{
+			TesterContainerNodeName: "node1",
+			TesterSource: ContainerIP{
+				IP: IP,
+				ContainerIdentifier: &provider.Container{
+					Data: &corev1.Container{
+						Name: containerName,
+					},
+				},
+			},
+			DestTargets: []ContainerIP{
+				{
+					IP: IP,
+					ContainerIdentifier: &provider.Container{
+						Data: &corev1.Container{
+							Name: containerName,
+						},
+					},
+				},
+			},
+		}
+	}
+
+	testCases := []struct {
+		testIP            string
+		testContainerName string
+		expectedString    string
+	}{
+		{
+			testIP:            "192.168.1.1",
+			testContainerName: "container1",
+			expectedString:    "From initiating container: 192.168.1.1 ( node:  ns:  podName:  containerName: container1 containerUID:  containerRuntime:  )\n--> To target container: 192.168.1.1 ( node:  ns:  podName:  containerName: container1 containerUID:  containerRuntime:  )\n", //nolint:lll
+		},
+		{
+			testIP:            "192.168.1.2",
+			testContainerName: "container2",
+			expectedString:    "From initiating container: 192.168.1.2 ( node:  ns:  podName:  containerName: container2 containerUID:  containerRuntime:  )\n--> To target container: 192.168.1.2 ( node:  ns:  podName:  containerName: container2 containerUID:  containerRuntime:  )\n", //nolint:lll
+		},
+	}
+
+	for _, tc := range testCases {
+		ntc := buildTestContext(tc.testIP, tc.testContainerName)
+		assert.Equal(t, tc.expectedString, ntc.String())
+	}
+}
+
+func TestNetTestContextMapString(t *testing.T) {
+	buildTestContext := func(IP, containerName string) NetTestContext {
+		return NetTestContext{
+			TesterContainerNodeName: "node1",
+			TesterSource: ContainerIP{
+				IP: IP,
+				ContainerIdentifier: &provider.Container{
+					Data: &corev1.Container{
+						Name: containerName,
+					},
+				},
+			},
+			DestTargets: []ContainerIP{
+				{
+					IP: IP,
+					ContainerIdentifier: &provider.Container{
+						Data: &corev1.Container{
+							Name: containerName,
+						},
+					},
+				},
+			},
+		}
+	}
+
+	testCases := []struct {
+		testIndex         string
+		testIP            string
+		testContainerName string
+		expectedString    string
+	}{
+		{
+			testIndex:         "test1",
+			testIP:            "192.168.1.1",
+			testContainerName: "container1",
+			expectedString:    "***Test for Network attachment: test1\nFrom initiating container: 192.168.1.1 ( node:  ns:  podName:  containerName: container1 containerUID:  containerRuntime:  )\n--> To target container: 192.168.1.1 ( node:  ns:  podName:  containerName: container1 containerUID:  containerRuntime:  )\n\n", //nolint:lll
+		},
+		{
+			testIndex:         "test2",
+			testIP:            "192.168.1.2",
+			testContainerName: "container2",
+			expectedString:    "***Test for Network attachment: test2\nFrom initiating container: 192.168.1.2 ( node:  ns:  podName:  containerName: container2 containerUID:  containerRuntime:  )\n--> To target container: 192.168.1.2 ( node:  ns:  podName:  containerName: container2 containerUID:  containerRuntime:  )\n\n", //nolint:lll
+		},
+	}
+
+	for _, tc := range testCases {
+		m := make(map[string]NetTestContext)
+		m[tc.testIndex] = buildTestContext(tc.testIP, tc.testContainerName)
+		assert.Equal(t, tc.expectedString, PrintNetTestContextMap(m))
 	}
 }
