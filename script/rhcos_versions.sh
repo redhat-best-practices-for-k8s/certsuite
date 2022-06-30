ce8a8e0579e000f193b632619efa8f9ad27fb85d
Brandon Palm <bpalm@redhat.com>
2022-06-30 11:26:59 -0500

    Address comments; add set -e; remove quotes

--- script/rhcos_versions.sh
+++ script/rhcos_versions.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
 set -x
+set -e
 
-CHANNELS=("4.10" "4.9" "4.8" "4.7" "4.6" "4.5" "4.4" "4.3" "4.2" "4.1")
+CHANNELS=(4.10 4.9 4.8 4.7 4.6 4.5 4.4 4.3 4.2 4.1)
 
 rm -f ./rhcos_version_map &> /dev/null
 
-for i in ${CHANNELS[@]}; do
+for i in "${CHANNELS[@]}"; do
     VERSIONS=$(curl -sH 'Accept: application/json'  "https://api.openshift.com/api/upgrades_info/v1/graph?channel=stable-${i}" | jq '.nodes[].version' -r)
 
     for VERSION in ${VERSIONS}
     do
         # Look up the release version using oc adm.
         RHCOSVERSION="$(oc adm release info "${VERSION}" -o 'jsonpath={.displayVersions.machine-os.Version}')"
-        if [ ! -z ${RHCOSVERSION} ]; then
+        if [[ -n ${RHCOSVERSION} ]]; then
             echo "$VERSION / $RHCOSVERSION" | tee -a ./rhcos_version_map
         fi
     done
@@ -21,4 +22,4 @@ done
 
 sort -o ./rhcos_version_map ./rhcos_version_map 
 
-echo OpenShift to RHCOS version mapping is in 'rhcos_version_map'
+echo OpenShift to RHCOS version mapping is in rhcos_version_map
