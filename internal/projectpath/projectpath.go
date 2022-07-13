77de1b1cc479c75831e24f3cf13ca2360a3ff4da
Brandon Palm <bpalm@redhat.com>
2022-07-13 13:24:34 -0500

    Fix projectpath to handle proper workingdir (#341)

--- internal/projectpath/projectpath.go
+++ internal/projectpath/projectpath.go
@@ -1,14 +1,25 @@
 package projectpath
 
 import (
+	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 )
 
 var (
 	_, b, _, _ = runtime.Caller(0)
-
-	// Root folder of this project
-	//nolint:gocritic
-	Root = filepath.Join(filepath.Dir(b), "../..")
+	Root       = getCorrectPath()
 )
+
+func getCorrectPath() string {
+	if os.Getenv("UNIT_TEST") != "" || strings.HasSuffix(os.Args[0], ".test") || strings.Contains(os.Args[0], "/T/") {
+		// Root folder of this project used for unit testing
+		//nolint:gocritic
+		return filepath.Join(filepath.Dir(b), "../..")
+	}
+
+	// The real working directory of anything non unit test related
+	path, _ := os.Getwd()
+	return path
+}
