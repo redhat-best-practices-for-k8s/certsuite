c0e5d6e270537bbc0d92fc83d576d12cedd4001c
Gonzalo Reyero Ferreras <greyerof@redhat.com>
2022-09-14 11:05:21 +0000

    Add label common as default when no labels provided.
    This is a backwards-compatible change as all the TCs have the "common"
    label already.
    
    This will prevent TCs marked as "extended", or any other custom label,
    to not be part (yet) of the official release.
    
    Signed-off-by: Gonzalo Reyero Ferreras <greyerof@redhat.com>

--- run-cnf-suites.sh
+++ run-cnf-suites.sh
@@ -118,9 +118,13 @@ if [ -n "$FOCUS" ]; then
 	fi
 fi
 
-if [ -n "$LABEL" ]; then
-    LABEL_STRING="-ginkgo.label-filter=${LABEL}"
+# Add the label "common" in case no labels have been provided. This will allow to filter out
+# some either non-official TCs or TCs not intended to run in CI (yet).
+# ToDo: remove when "-f" and "-s" flags have been deprecated.
+if [ -z "$LABEL" ]; then
+	LABEL="common"
 fi
+LABEL_STRING="-ginkgo.label-filter=${LABEL}"
 
 if [ -z "$FOCUS_STRING" ] && [ -z "$LABEL_STRING" ]; then
 	echo "No test focus (-f) or label (-l) was set, so only diagnostic functions will run.".
@@ -128,7 +132,7 @@ fi
 
 
 
-cd ./cnf-certification-test && ./cnf-certification-test.test ${FOCUS_STRING} ${SKIP_STRING} ${LABEL_STRING:+"${LABEL_STRING}"} ${GINKGO_ARGS}
+cd ./cnf-certification-test && ./cnf-certification-test.test ${FOCUS_STRING} ${SKIP_STRING} ${LABEL_STRING} ${GINKGO_ARGS}
 
 # if [[ ! -z "${TNF_PARTNER_SRC_DIR}" ]]; then
 # 	echo "attempting to delete litmus"
