01523bdd710fedc8c6485e259e41856741a03fab
Brandon Palm <bpalm@redhat.com>
2023-01-23 13:01:33 -0600

    Make the LOCAL_DOCKERCFG variable optional

--- run-tnf-container.sh
+++ run-tnf-container.sh
@@ -14,10 +14,9 @@
 #   KUBECONFIG=/usr/tnf/kubeconfig/config:/usr/tnf/kubeconfig/config.2
 
 export REQUIRED_NUM_OF_ARGS=5
-export REQUIRED_VARS=('LOCAL_KUBECONFIG' 'LOCAL_TNF_CONFIG' 'OUTPUT_LOC' 'LOCAL_DOCKERCFG')
+export REQUIRED_VARS=('LOCAL_KUBECONFIG' 'LOCAL_TNF_CONFIG' 'OUTPUT_LOC')
 export REQUIRED_VARS_ERROR_MESSAGES=(
 	'KUBECONFIG is invalid or not given. Use the -k option to provide path to one or more kubeconfig files.'
-	'DOCKERCFG is invalid or not given. Use the -c option to provide path to one or more dockercfg files.'
 	'TNFCONFIG is required. Use the -t option to specify the directory containing the TNF configuration files.'
 	'OUTPUT_LOC is required. Use the -o option to specify the output location for the test results.'
 )
@@ -130,6 +129,8 @@ perform_kubeconfig_autodiscovery() {
 }
 
 perform_dockercfg_autodiscovery() {
+  # As of now, the Docker Config is an optional variable to be supplied as the only test suite that
+  # requires it is the preflight suite.
   # See the openshift-preflight documentation about environment variables
   # https://github.com/redhat-openshift-ecosystem/openshift-preflight/blob/main/docs/CONFIG.md#container-policy-configuration
   if [[ -n "$PFLT_DOCKERCONFIG" ]]; then
