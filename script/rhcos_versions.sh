50b5d2bca56b12f817b43c1e6c367bfb37e5c2dd
Ramon Perez <raperez@redhat.com>
2023-03-15 17:41:01 +0100

    Update script and add warning for cscos

--- script/rhcos_versions.sh
+++ script/rhcos_versions.sh
@@ -3,19 +3,22 @@
 set -x
 set -e
 
-CHANNELS=(4.12 4.11 4.10 4.9 4.8 4.7 4.6 4.5 4.4 4.3 4.2 4.1)
+CHANNELS=(4.13 4.12 4.11 4.10 4.9 4.8 4.7 4.6 4.5 4.4 4.3 4.2 4.1)
+CHANNEL_TYPES=(stable candidate)
 
 rm -f ./cnf-certification-test/platform/operatingsystem/files/rhcos_version_map &>/dev/null
 
 for i in "${CHANNELS[@]}"; do
-	VERSIONS=$(curl -sH 'Accept: application/json' "https://api.openshift.com/api/upgrades_info/v1/graph?channel=stable-${i}" | jq '.nodes[].version' -r)
+	for j in "${CHANNEL_TYPES[@]}"; do
+		VERSIONS=$(curl -sH 'Accept: application/json' "https://api.openshift.com/api/upgrades_info/v1/graph?channel=${j}-${i}" | jq '.nodes[].version' -r)
 
-	for VERSION in ${VERSIONS}; do
-		# Look up the release version using oc adm.
-		RHCOSVERSION="$(oc adm release info "${VERSION}" -o 'jsonpath={.displayVersions.machine-os.Version}')"
-		if [[ -n ${RHCOSVERSION} ]]; then
-			echo "$VERSION / $RHCOSVERSION" | tee -a ./cnf-certification-test/platform/operatingsystem/files/rhcos_version_map
-		fi
+		for VERSION in ${VERSIONS}; do
+			# Look up the release version using oc adm.
+			RHCOSVERSION="$(oc adm release info "${VERSION}" -o 'jsonpath={.displayVersions.machine-os.Version}')"
+			if [[ -n ${RHCOSVERSION} ]]; then
+				echo "$VERSION / $RHCOSVERSION" | tee -a ./cnf-certification-test/platform/operatingsystem/files/rhcos_version_map
+			fi
+		done
 	done
 done
 
