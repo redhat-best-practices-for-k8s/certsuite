36f063ef363f977acf4cd5a66daf0bfc63428df5
Brandon Palm <bpalm@redhat.com>
2023-03-23 09:42:16 -0500

    Fix RHCOS script

--- script/rhcos_versions.sh
+++ script/rhcos_versions.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 set -x
-set -e
 
 CHANNELS=(4.14 4.13 4.12 4.11 4.10 4.9 4.8 4.7 4.6 4.5 4.4 4.3 4.2 4.1)
 CHANNEL_TYPES=(stable candidate)
@@ -13,18 +12,9 @@ for i in "${CHANNELS[@]}"; do
 		VERSIONS=$(curl -sH 'Accept: application/json' "https://api.openshift.com/api/upgrades_info/v1/graph?channel=${j}-${i}" | jq '.nodes[].version' -r)
 		for VERSION in ${VERSIONS}; do
 			# Look up the release version using oc adm.
-			if RHCOSVERSION="$(
-				oc adm release \
-					info "${VERSION}" \
-					-o 'jsonpath={.displayVersions.machine-os.Version}' \
-					2>&1
-			)"; then
-				if [[ -n ${RHCOSVERSION} ]]; then
-					echo "$VERSION / $RHCOSVERSION" |
-						tee -a ./cnf-certification-test/platform/operatingsystem/files/rhcos_version_map
-				fi
-			else
-				printf 'Continue with the error %s.\n' "$RHCOSVERSION"
+			RHCOSVERSION="$(oc adm release info "${VERSION}" -o 'jsonpath={.displayVersions.machine-os.Version}')"
+			if [[ -n ${RHCOSVERSION} ]]; then
+				echo "$VERSION / $RHCOSVERSION" | tee -a ./cnf-certification-test/platform/operatingsystem/files/rhcos_version_map
 			fi
 		done
 	done
