9b7ef03ad0f36c81f4379b6dac1e2dc71e65dabe
aabughosh <aabugosh@redhat.com>
2023-03-30 14:59:48 +0300

    update the classification read file

---
+++ script/classification.js
@@ -0,0 +1,685 @@
+classification = {
+"classification" : {
+"networking-dpdk-cpu-pinning-exec-probe":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"networking-dual-stack-service":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"networking-icmpv4-connectivity":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"networking-icmpv4-connectivity-multus":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"networking-icmpv6-connectivity":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"networking-icmpv6-connectivity-multus":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"networking-network-policy-deny-all":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Optional",
+"ForFarEdge": "Optional"
+
+	}
+],"networking-ocp-reserved-ports-usage":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"networking-reserved-partner-ports":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"networking-restart-on-reboot-sriov-pod":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"networking-service-type":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"networking-undeclared-container-ports-usage":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Optional"
+
+	}
+],"observability-container-logging":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"observability-crd-status":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"observability-pod-disruption-budget":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"observability-termination-policy":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Optional",
+"ForFarEdge": "Optional"
+
+	}
+],"access-control-cluster-role-bindings":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"access-control-container-host-port":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Optional",
+"ForFarEdge": "Optional"
+
+	}
+],"access-control-ipc-lock-capability-check":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"access-control-namespace":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"access-control-namespace-resource-quota":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Optional"
+
+	}
+],"access-control-net-admin-capability-check":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"access-control-net-raw-capability-check":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"access-control-no-1337-uid":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Optional"
+
+	}
+],"access-control-one-process-per-container":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Optional",
+"ForFarEdge": "Optional"
+
+	}
+],"access-control-pod-automount-service-account-token":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"access-control-pod-host-ipc":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"access-control-pod-host-network":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"access-control-pod-host-path":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"access-control-pod-host-pid":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"access-control-pod-role-bindings":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"access-control-pod-service-account":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"access-control-requests-and-limits":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"access-control-security-context":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Optional",
+"ForFarEdge": "Optional"
+
+	}
+],"access-control-security-context-non-root-user-check":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Optional",
+"ForFarEdge": "Optional"
+
+	}
+],"access-control-security-context-privilege-escalation":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Optional",
+"ForFarEdge": "Optional"
+
+	}
+],"access-control-ssh-daemons":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"access-control-sys-admin-capability-check":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"access-control-sys-nice-realtime-capability":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"access-control-sys-ptrace-capability":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"manageability-container-port-name-format":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Optional"
+
+	}
+],"manageability-containers-image-tag":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Optional",
+"ForFarEdge": "Optional"
+
+	}
+],"lifecycle-affinity-required-pods":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Optional"
+
+	}
+],"lifecycle-container-shutdown":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"lifecycle-container-startup":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"lifecycle-cpu-isolation":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"lifecycle-crd-scaling":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Optional"
+
+	}
+],"lifecycle-deployment-scaling":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Optional"
+
+	}
+],"lifecycle-image-pull-policy":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Optional",
+"ForFarEdge": "Optional"
+
+	}
+],"lifecycle-liveness-probe":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"lifecycle-persistent-volume-reclaim-policy":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Optional",
+"ForFarEdge": "Optional"
+
+	}
+],"lifecycle-pod-high-availability":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"lifecycle-pod-owner-type":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"lifecycle-pod-recreation":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"lifecycle-pod-scheduling":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"lifecycle-pod-toleration-bypass":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"lifecycle-readiness-probe":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"lifecycle-startup-probe":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"lifecycle-statefulset-scaling":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Optional"
+
+	}
+],"lifecycle-storage-required-pods":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"operator-install-source":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"operator-install-status-no-privileges":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"operator-install-status-succeeded":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"performance-exclusive-cpu-pool":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"performance-exclusive-cpu-pool-rt-scheduling-policy":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Optional",
+"ForFarEdge": "Mandatory"
+
+	}
+],"performance-isolated-cpu-pool-rt-scheduling-policy":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Optional",
+"ForFarEdge": "Mandatory"
+
+	}
+],"performance-rt-apps-no-exec-probes":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Optional",
+"ForFarEdge": "Mandatory"
+
+	}
+],"performance-shared-cpu-pool-non-rt-scheduling-policy":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Optional",
+"ForFarEdge": "Mandatory"
+
+	}
+],"platform-alteration-base-image":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"platform-alteration-boot-params":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"platform-alteration-hugepages-1g-only":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Optional",
+"ForFarEdge": "Mandatory"
+
+	}
+],"platform-alteration-hugepages-2m-only":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Optional"
+
+	}
+],"platform-alteration-hugepages-config":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"platform-alteration-is-selinux-enforcing":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"platform-alteration-isredhat-release":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"platform-alteration-ocp-lifecycle":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"platform-alteration-ocp-node-os-lifecycle":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"platform-alteration-service-mesh-usage":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Optional",
+"ForFarEdge": "Optional"
+
+	}
+],"platform-alteration-sysctl-config":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"platform-alteration-tainted-node-kernel":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"affiliated-certification-container-is-certified":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"affiliated-certification-container-is-certified-digest":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"affiliated-certification-helmchart-is-certified":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"affiliated-certification-operator-is-certified":[
+	{
+"ForTelco": "Mandatory",
+"ForNonTelco": "Mandatory",
+"ForExtended": "Mandatory",
+"ForFarEdge": "Mandatory"
+
+	}
+],"chaostesting-pod-delete":[
+	{
+"ForTelco": "Optional",
+"ForNonTelco": "Optional",
+"ForExtended": "Optional",
+"ForFarEdge": "Optional"
+
+	}
+],
+}
+}
