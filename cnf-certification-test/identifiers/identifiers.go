a2249d75f3b1e9b92e2af23f148b016acec54451
bnshr <bmandal@redhat.com>
2023-06-21 11:34:17 +0200

    Mark pod-role-binding as implemented

--- cnf-certification-test/identifiers/identifiers.go
+++ cnf-certification-test/identifiers/identifiers.go
@@ -1021,7 +1021,7 @@ tag. (2) It doesn't have any of the following prefixes: default, openshift-, ist
 		PodRoleBindingsBestPracticesRemediation,
 		NoExceptions,
 		TestPodRoleBindingsBestPracticesIdentifierDocLink,
-		false,
+		true,
 		map[string]string{
 			FarEdge:  Mandatory,
 			Telco:    Mandatory,
