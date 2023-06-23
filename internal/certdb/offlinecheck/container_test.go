ebad7ebed2eb2770b02581a4827211621764eb36
David Elie-Dit-Cosaque <86730676+edcdavid@users.noreply.github.com>
2023-06-23 08:13:40 -0500

    Fix for hardcoded registry registry.redhat.io (#1187)
    * Fix for hardcoded registry registry.redhat.io
    
    * remove date check
    
    * add Documentation Note
    
    * Addressing Brandon's comments
    
    * lint

--- internal/certdb/offlinecheck/container_test.go
+++ internal/certdb/offlinecheck/container_test.go
@@ -29,7 +29,7 @@ const containersDBJSON = `{
 	   "_id":"",
 	   "architecture":"amd64",
 	   "certified":false,
-	   "docker_image_digest":"sha256:d2f388e163a5126f7112757f0475c8c4e036fe00c76ef8a7fd50848fafdcb96b",
+	   "image_id":"sha256:d2f388e163a5126f7112757f0475c8c4e036fe00c76ef8a7fd50848fafdcb96b",
 	   "docker_image_id":"",
 	   "repositories":[
 		  {
@@ -56,7 +56,7 @@ const containersDBJSON = `{
 	   "_id":"",
 	   "architecture":"amd64",
 	   "certified":false,
-	   "docker_image_digest":"sha256:fa8f2136aed9daf4c5a805068a87dd274016b8dddae36bc0b02e18b391690493",
+	   "image_id":"sha256:fa8f2136aed9daf4c5a805068a87dd274016b8dddae36bc0b02e18b391690493",
 	   "docker_image_id":"",
 	   "repositories":[
 		  {
