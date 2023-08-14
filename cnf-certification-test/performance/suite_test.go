099c25299630214a03def8ed6bcf6ba0cf5f4b67
shimritproj <shimritp74@gmail.com>
2023-08-14 21:17:55 +0300

    Adding unit tests and update my branch

--- cnf-certification-test/performance/suite_test.go
+++ cnf-certification-test/performance/suite_test.go
@@ -15,3 +15,43 @@
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 package performance
+
+import (
+	"testing"
+
+	"github.com/test-network-function/cnf-certification-test/pkg/provider"
+	"github.com/test-network-function/cnf-certification-test/pkg/testhelper"
+	"gotest.tools/v3/assert"
+)
+
+func Test_appendToCompliantObject(t *testing.T) {
+	testCases := []struct {
+		compliantObjects       []*testhelper.ReportObject
+		cut                    *provider.Container
+		put                    *provider.Pod
+		resultCompliantObjects []*testhelper.ReportObject
+	}{
+		{},
+		{},
+	}
+
+	for _, tc := range testCases {
+		assert.Equal(t, tc.resultCompliantObjects, appendToCompliantObject(tc.compliantObjects, tc.cut, tc.put))
+	}
+}
+
+func Test_appendTononCompliantObject(t *testing.T) {
+	testCases := []struct {
+		noncompliantObjects       []*testhelper.ReportObject
+		cut                       *provider.Container
+		put                       *provider.Pod
+		resultnonCompliantObjects []*testhelper.ReportObject
+	}{
+		{},
+		{},
+	}
+
+	for _, tc := range testCases {
+		assert.Equal(t, tc.resultnonCompliantObjects, appendToCompliantObject(tc.noncompliantObjects, tc.cut, tc.put))
+	}
+}
