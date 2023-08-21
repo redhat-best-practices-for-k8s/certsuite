4325b05b2612ab6a4d97915f9090e958b19ed9d0
jmontesi <jmontesi@redhat.com>
2023-08-21 14:49:45 +0200

    Add tnf.Log() function to allow log level selection
    It avoids the common pattern of calling tnf.ClaimFilePrint() followed or
    preceded by a logrus function to print a log with a different level than
    TRACE. It also avoids log duplication when doing this.
    
    The issue is fixed in a couple of places as an example, not with the
    intent of being exhaustive.

--- pkg/tnf/status.go
+++ pkg/tnf/status.go
@@ -9,11 +9,16 @@ import (
 
 // ClaimFilePrintf prints to claim and junit report files.
 func ClaimFilePrintf(format string, args ...interface{}) {
+	Log(logrus.TraceLevel, format, args)
+}
+
+// Log prints to stdout and claim and junit report files.
+func Log(level logrus.Level, format string, args ...interface{}) {
 	message := fmt.Sprintf(format+"\n", args...)
 	_, err := ginkgo.GinkgoWriter.Write([]byte(message))
 	if err != nil {
 		logrus.Errorf("Ginkgo writer could not write msg '%s' because: %s", message, err)
-	} else {
-		logrus.Trace(message)
 	}
+
+	logrus.StandardLogger().Log(level, message)
 }
