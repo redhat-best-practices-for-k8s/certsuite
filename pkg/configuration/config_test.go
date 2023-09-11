3825e75c5b7d67154e1138b1469a16e20d6c3355
jmontesi <100689165+jmontesi@users.noreply.github.com>
2023-09-11 17:26:38 +0200

    config: use a string slice to hold the target namespaces (#1389)
    * config: use a string slice to hold the target namespaces
    
    It substitutes the previous struct which only had one field for the
    namespace name.
    
    Also, targetNamespaces is used instead of targetNameSpaces in the
    config file.
    
    * Revert a non backward compatible change

--- pkg/configuration/config_test.go
+++ pkg/configuration/config_test.go
@@ -35,10 +35,8 @@ func TestLoadConfiguration(t *testing.T) {
 	assert.Nil(t, err)
 	// check if targetNameSpaces section is parsed properly
 	assert.Equal(t, nsLength, len(env.TargetNameSpaces))
-	ns := configuration.Namespace{Name: ns1}
-	assert.Contains(t, env.TargetNameSpaces, ns)
-	ns.Name = ns2
-	assert.Contains(t, env.TargetNameSpaces, ns)
+	assert.Contains(t, env.TargetNameSpaces, ns1)
+	assert.Contains(t, env.TargetNameSpaces, ns2)
 	// check if targetPodlabels section is parsed properly
 	assert.Equal(t, labels, len(env.TargetPodLabels))
 	podlabel1 := configuration.Label{Prefix: label1Prefix, Name: label1Name, Value: label1Value}
