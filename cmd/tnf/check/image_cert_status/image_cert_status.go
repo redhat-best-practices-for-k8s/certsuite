11330064b5543c3234878c5b9e55267482c8deac
jmontesi <jmontesi@redhat.com>
2023-09-19 17:35:34 +0200

    Check flags compatibility

--- cmd/tnf/check/image_cert_status/image_cert_status.go
+++ cmd/tnf/check/image_cert_status/image_cert_status.go
@@ -24,51 +24,55 @@ import (
 	"github.com/test-network-function/cnf-certification-test/internal/certdb"
 )
 
-// generateCmd is the root of the "catalog generate" CLI program.
 var checkImageCertStatusCmd = &cobra.Command{
 	Use:   "image-cert-status",
 	Short: "Verifies the container's image certification status",
 	RunE:  checkImageCertStatus,
 }
 
-func checkImageCertStatus(cmd *cobra.Command, args []string) error {
+func checkImageCertStatus(cmd *cobra.Command, _ []string) error {
 	imageName, _ := cmd.Flags().GetString("name")
 	imageRegistry, _ := cmd.Flags().GetString("registry")
 	imageTag, _ := cmd.Flags().GetString("tag")
 	imageDigest, _ := cmd.Flags().GetString("digest")
-	offlineDb, _ := cmd.Flags().GetString("offline-db")
+	offlineDB, _ := cmd.Flags().GetString("offline-db")
 
-	validator, err := certdb.GetValidator(offlineDb)
+	validator, err := certdb.GetValidator(offlineDB)
 	if err != nil {
 		return fmt.Errorf("could not get a validator for container images, error: %v", err)
 	}
 
-	if imageName != "" {
+	switch {
+	case imageName != "":
 		fmt.Println("**********************************************")
 		fmt.Printf("Image name: %s\nImage registry: %s\nImage tag: %s\n", imageName, imageRegistry, imageTag)
 		fmt.Println("**********************************************")
-	} else {
+	case imageDigest != "":
 		fmt.Println("**************************************************************************************")
 		fmt.Printf("Image digest: %s\n", imageDigest)
 		fmt.Println("**************************************************************************************")
+	default:
+		return fmt.Errorf("either an image name or an image digest must be provided")
 	}
 
 	if validator.IsContainerCertified(imageRegistry, imageName, imageTag, imageDigest) {
-		color.Green("Image certified")
+		fmt.Printf("Result: %s\n", color.GreenString("Image certified"))
 	} else {
-		color.Red("Image not certified")
+		fmt.Printf("Result: %s\n", color.RedString("Image not certified"))
 	}
 
 	return nil
 }
 
-// Execute executes the "catalog" CLI.
 func NewCommand() *cobra.Command {
 	checkImageCertStatusCmd.PersistentFlags().String("name", "", "name of the image to verify")
 	checkImageCertStatusCmd.PersistentFlags().String("registry", "", "registry where the image is stored")
 	checkImageCertStatusCmd.PersistentFlags().String("tag", "latest", "image tag to be fetched")
 	checkImageCertStatusCmd.PersistentFlags().String("digest", "", "digest of the image")
 	checkImageCertStatusCmd.PersistentFlags().String("offline-db", "", "path to the offline db (for disconnected environments)")
+
+	checkImageCertStatusCmd.MarkFlagsRequiredTogether("name", "registry")
+	checkImageCertStatusCmd.MarkFlagsMutuallyExclusive("name", "digest")
 
 	return checkImageCertStatusCmd
 }
