e5c399409e45fdbd5a63c2e52153ea677faa0977
jmontesi <100689165+jmontesi@users.noreply.github.com>
2023-09-21 12:43:51 +0200

    config: delete deprecated field targetPodLabels (#1390)

--- cnf-certification-test/chaostesting/pod_delete/pod_delete.go
+++ cnf-certification-test/chaostesting/pod_delete/pod_delete.go
@@ -15,6 +15,8 @@
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 package poddelete
 
+/*
+
 import (
 	"bufio"
 	"bytes"
@@ -292,3 +294,5 @@ func createResource(decoder *yamlutil.YAMLOrJSONDecoder) error {
 		}
 	}
 }
+
+*/
