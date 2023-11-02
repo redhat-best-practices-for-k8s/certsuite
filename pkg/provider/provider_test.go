3bf3b3cff86d10ef5eef8058a13584580a271448
Brandon Palm <bpalm@redhat.com>
2023-11-02 11:45:40 -0500

    Prepare for v4.5.3 (#1578)

--- pkg/provider/provider_test.go
+++ pkg/provider/provider_test.go
@@ -788,7 +788,7 @@ func TestBuildImageWithVersion(t *testing.T) {
 		{
 			repoVar:         "",
 			supportImageVar: "",
-			expectedOutput:  "quay.io/testnetworkfunction/debug-partner:4.5.2",
+			expectedOutput:  "quay.io/testnetworkfunction/debug-partner:4.5.3",
 		},
 	}
 
