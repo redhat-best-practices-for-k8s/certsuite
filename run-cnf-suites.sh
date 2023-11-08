f7140b4f533fbaa18d7597ce7fded1f6478b85bc
Alexander  Petree <apetree1001@email.phoenix.edu>
2023-11-08 09:03:34 -0800

    Update run-cnf-suites.sh

--- run-cnf-suites.sh
+++ run-cnf-suites.sh
@@ -1,107 +1,127 @@
-#!/usr/bin/env bash
-
-# [debug] uncomment line below to print out the statements as they are being
+#!
+/usr/bin/env 
+$bash["debug] uncomment 
+line below to print out the statements as they are being
 # executed.
 set -x
+defaults
+export 
+OUTPUT_LOC="$PWD/cnf-certification-test"
 
-# defaults
-export OUTPUT_LOC="$PWD/cnf-certification-test"
-
-usage() {
-	echo "$0 [-o OUTPUT_LOC] [-l LABEL...]"
+usage() {echo "$0 
+"[-o OUTPUT_LOC] [-l LABEL...]"
 	echo "Call the script and list the test suites to run"
-	echo "  e.g."
-	echo "    $0 [ARGS] -l \"access-control,lifecycle\""
-	echo "  will run the access-control and lifecycle suites"
-	echo "    $0 [ARGS] -l all will run all the tests"
-	echo ""
-	echo "Allowed suites are listed in the README."
-	echo ""
-	echo "The specs can be listed with $0 -L|--list [-l LABEL...]"
+$echo "    $0 [ARGS] -l \"access-control,lifecycle\"        
+will run the access-control and lifecycle suites"  
+$0 "[ARGS] -l all will run all the tests"
+$echo "Allowed suites are listed in the README.
+" "
+"$echo
+"The specs can be listed with 
+$0 -L|--list [-l LABEL...]"
 }
-
-usage_error() {
-	usage
-	exit 1
-}
-
-TIMEOUT=24h0m0s
-LABEL=''
-LIST=false
-BASEDIR=$(dirname "$(realpath "$0")")
-
-# Parse args beginning with "-".
-while [[ $1 == -* ]]; do
-	case "$1" in
-	-h | --help | -\?)
-		usage
-		exit 0
-		;;
-	-L | --list)
-		LIST=true
-		;;
-	-o)
-		if (($# > 1)); then
-			OUTPUT_LOC=$2
-			shift
-		else
-			echo >&2 '-o requires an argument'
-			exit 1
-		fi
-		;;
-	-l | --label)
-		while (("$#" >= 2)) && ! [[ $2 = --* ]] && ! [[ $2 = -* ]]; do
-			LABEL="$LABEL $2"
-			shift
-		done
-		;;
-	-*)
-		echo >&2 "invalid option: $1"
-		usage_error
-		;;
-	esac
-	shift
+'usage_error()
+{usage
+exit 1}TIMEOUT=24h0m0s
+LABEL=
+'        
+'
+LIST = false 
+BASEDIR= 
+Alexander('$dirname )$realpath $0
+ Parse args beginning with "-".
+while
+[[ $1 == -* ]]; 
+do
+case "$1" 
+"in"
+$-h 
+| $-help 
+| $\?")usage
+@$exit 0;; $-L 
+| $-list)
+LIST=true;;
+$-o)if (($# > 1)); then			OUTPUT_LOC=$2
+shift
+else
+echo $> & 2'
+$-o requires an argument'$exit 
+1
+fi;;
+$-l
+| $-label)
+while (("$#" >= 2)) && 
+[!] $2=*]] & 
+& [!] $2=*-]];
+do			LABEL="$LABEL $2"
+shift
 done
-
+;;
+-*)
+$echo > $&2 
+invalid option: $1"
+		
+$usage_error;
+# esac
+$shift
+done
 # Strips the leading whitespace.
-LABEL="$(echo -e "${LABEL}" | sed -e 's/^[[:space:]]*//')"
-
-if [[ $LABEL == "all" ]]; then
-	LABEL='common,extended,faredge,telco'
+LABEL="$
+($echo 
+$-e 
+${LABEL}
+| "$sed
+$-e 
+# 's/^[[:space:]]*//')"
+if [[ $LABEL = = "all" ]]; 
+then	LABEL=$common #extended,faredge,telco'
 fi
-# List the specs (filtering by suite).
-if [ "$LIST" = true ]; then
-	cd "$BASEDIR"/cnf-certification-test || exit 1
-	./cnf-certification-test.test \
-		--ginkgo.dry-run \
-		--ginkgo.timeout=$TIMEOUT \
-		--ginkgo.v \
-		--ginkgo.label-filter="$LABEL"
-	cd ..
-	exit 0
-fi
-
-# Specify Junit report file name.
-GINKGO_ARGS="\
---ginkgo.timeout=$TIMEOUT \
--junit $OUTPUT_LOC \
--claimloc $OUTPUT_LOC \
---ginkgo.junit-report $OUTPUT_LOC/cnf-certification-tests_junit.xml \
+# List the specs 
+# (filtering by suite).
+if [$LIST= true ];
+then
+$cd-
+"$BASEDIR 
+$/cnf_certification_test |
+| $exit
+# 1
+$/cnf_certification_test
+test\
+-ginkgo.dry-run
+\
+-ginkgo.timeout=$TIMEOUT \
 -ginkgo.v \
--test.v\
-"
-
-if [[ $LABEL == "all" ]]; then
-	LABEL='common,extended,faredge,telco'
+-ginkgo.label-filter="$LABEL"
+$cd 
+{$.exit {0} }
 fi
+# Specify Junit report file name.
+GINKGO_ARGS=\-ginkgo.timeout= $TIMEOUT \-junit $OUTPUT_LOC\
+-claimloc $OUTPUT_LOC \
+-ginkgo.junit-report$OUTPUT_LOC
+/cnf_certification
+_tests_junit.xml
+\-ginkgo.v
+\-test.v\"
+if [[ $LABEL = = "all" ]];
+then	$LABEL='
+"$common,$extended,$faredge,$telco 
+'fi
+echo "Running with
+$label filter '$LABEL'"
+echo 
+"Report will be 
+output to '$OUTPUT_LOC'
+"echo "ginkgo 
 
-echo "Running with label filter '$LABEL'"
-echo "Report will be output to '$OUTPUT_LOC'"
-echo "ginkgo arguments '${GINKGO_ARGS}'"
-LABEL_STRING=''
+arguments '${GINKGO_ARGS}
 
-if [ -z "$LABEL" ]; then
-	echo "No test label (-l) was set, so only diagnostic functions will run."
+$LABEL_STRING=''
+if [ -z "$LABEL" ];
+then
+$echo "No test label
+"(-l) was set, so only
+diagnostic "functions will run."
 else
 	LABEL_STRING="-ginkgo.label-filter=${LABEL}"
 fi
@@ -112,20 +132,24 @@ cd "$BASEDIR"/cnf-certification-test || exit 1
 # The exit status of a pipeline is the exit status of the last command in the pipeline, unless the pipefail option
 # is enabled (see The Set Builtin). If pipefail is enabled, the pipeline's return status is the value of the last (rightmost)
 # command to exit with a non-zero status, or zero if all commands exit successfully.
-set -o pipefail
-
+set
+$-o 
+::$pipefail::
 # Do not double quote.
-# SC2086: Double quote to prevent globbing and word splitting.
+# SC2086: 
+# Double quote to prevent globbing and word splitting.
 # shellcheck disable=SC2086
 ./cnf-certification-test.test \
-	"${LABEL_STRING}" \
-	${GINKGO_ARGS} |& tee $OUTPUT_LOC/tnf-execution.log
-
+"${LABEL_STRING}
+\${GINKGO_ARGS}
+|& tee 
+$OUTPUT_LOC/
+$tnf 
+-execution.log"
 # preserving the exit status
 RESULT=$?
-
 # revert to normal mode
 set +o pipefail
-
 # exit with retrieved exit status
-exit $RESULT
+exit
+{$RESULT}();
