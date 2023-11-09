3ed08380523e5554f33b46adc4e697b33c10ee4d
aabughosh <88486034+aabughosh@users.noreply.github.com>
2023-11-09 19:34:08 +0200

    website for our cnf (#1544)
    * website for our cnf
    
    to run need to call  ./run-cnf-suites.sh -l lifecycle -s true
    -s new filed mean if i want to run that from the website
    
    * a few changes(add upload file) and added the -s for server
    
    * change folder name
    
    * Delete index.html
    
    * added new function and rename names
    
    * add new file for html function
    
    * fix for saving the log to be in a bif and not in a file
    
    * add bufer implemntation fir logs
    
    * update the flag
    
    * few changes for linter
    
    * Update run-cnf-suites.sh
    
    * udpate typos
    
    * david comments
    
    * update the flag
    
    * website for our cnf
    
    to run need to call  ./run-cnf-suites.sh -l lifecycle -s true
    -s new filed mean if i want to run that from the website
    
    * a few changes(add upload file) and added the -s for server
    
    * change folder name
    
    * Delete index.html
    
    * added new function and rename names
    
    * add new file for html function
    
    * fix for saving the log to be in a bif and not in a file
    
    * add bufer implemntation fir logs
    
    * update the flag
    
    * few changes for linter
    
    * Update run-cnf-suites.sh
    
    * udpate typos
    
    * david comments
    
    * update the flag

---
+++ cnf-certification-test/webserver/toast.js
@@ -0,0 +1,53 @@
+import '@rhds/elements/rh-alert/rh-alert.js';
+
+export async function toast({
+  heading,
+  message,
+  state = 'info',
+  timeout = 8_000,
+}) {
+  await import('@rhds/elements/rh-alert/rh-alert.js');
+  const h2 = document.createElement('h2');
+        h2.textContent = heading;
+        h2.slot = 'header';
+  const alert = document.createElement('rh-alert');
+        alert.setAttribute('aria-live', 'polite');
+        alert.dismissable = true;
+        alert.state = state;
+        alert.classList.add('toast');
+        alert.style.position = 'fixed';
+        alert.style.margin = '0';
+        alert.style.setProperty('z-index', '1000');
+        alert.style.setProperty('inset-inline-end', 'var(--rh-space-xl, 24px)');
+        alert.style.setProperty('inset-block-start', 'var(--rh-space-xl, 24px)');
+        alert.append(h2);
+  if (message) {
+    const p = document.createElement('p');
+          p.textContent = message;
+    alert.append(message);
+  }
+
+  alert.animate({ translate: ['100% 0', '0 0'] }, { duration: 200 });
+
+  await Promise.all(Array.from(document.querySelectorAll('rh-alert.toast'), toast =>
+    // TODO: handle more than 2 toasts
+    toast.animate({
+      translate: [
+        '0 auto',
+        '0 calc(100% + 20px)',
+      ],
+    }, {
+      duration: 200,
+      composite: 'accumulate',
+      rangeEnd: '100%',
+      fill: 'forwards',
+    }).finished));
+
+  setTimeout(() => {
+    if (alert.isConnected) {
+      alert.remove();
+    }
+  }, timeout);
+
+  document.body.append(alert);
+}
