6d52d79d2cf4627ce16365d52f9666ee77a4b531
jmontesi <jmontesi@redhat.com>
2023-11-15 16:12:11 +0100

    Irrelevant change to see if the GitHub actions are run

--- pkg/claim/schema_test.go
+++ pkg/claim/schema_test.go
@@ -112,7 +112,7 @@ func TestResult_MarshalJSON(t *testing.T) {
 		want    []byte
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		// TODO: Add test cases
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
