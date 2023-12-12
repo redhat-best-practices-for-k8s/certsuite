e572541b2c67b496f9b77a46ec9b236a12bed6b4
jmontesi <jmontesi@redhat.com>
2023-12-12 10:11:49 +0100

    testhelper: remove unused functions

--- pkg/testhelper/testhelper_test.go
+++ pkg/testhelper/testhelper_test.go
@@ -17,7 +17,6 @@
 package testhelper
 
 import (
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -36,74 +35,5 @@ func TestResultToString(t *testing.T) {
 
 	for _, tc := range testCases {
 		assert.Equal(t, tc.expectedResult, ResultToString(tc.input))
-	}
-}
-
-func TestSkipIfEmptyFuncs(t *testing.T) {
-	testCases := []struct {
-		map1, slice1           interface{}
-		skippedAny, skippedAll bool
-	}{
-		{ // Test Case #1 - Skip because objects is empty, no panic because []string type
-			slice1:     []string{},
-			map1:       make(map[string]string),
-			skippedAny: true,
-			skippedAll: true,
-		},
-		{ // Test Case #2 - Skip because objects is empty, no panic because map type
-			slice1:     make(map[string]string),
-			map1:       make(map[string]string),
-			skippedAny: true,
-			skippedAll: true,
-		},
-		{ // Test Case #3 - No skip because objects is populated, no panic because []string type
-			slice1:     []string{"test"},
-			map1:       make(map[string]string),
-			skippedAny: true,
-			skippedAll: false,
-		},
-		{ // Test Case #4 - Multiple objects
-			slice1:     []string{"test1", "test2"},
-			map1:       make(map[string]string),
-			skippedAny: true,
-			skippedAll: false,
-		},
-		{ // Test Case #5 - Nil objects panic
-			slice1:     nil,
-			map1:       nil,
-			skippedAny: true,
-			skippedAll: true,
-		},
-	}
-
-	for _, tc := range testCases {
-		result := false
-
-		if tc.slice1 == nil || tc.map1 == nil {
-			assert.Panics(t, func() {
-				SkipIfEmptyAll(func(s string, i ...int) {}, NewSkipObject(tc.slice1, "tc.slice1"), NewSkipObject(tc.map1, "tc.map1"))
-			})
-			assert.Panics(t, func() {
-				SkipIfEmptyAny(func(s string, i ...int) {}, NewSkipObject(tc.slice1, "tc.slice1"), NewSkipObject(tc.map1, "tc.map1"))
-			})
-		} else {
-			SkipIfEmptyAll(func(s string, i ...int) {
-				if strings.Contains(s, "Test skipped") {
-					result = true
-				} else {
-					result = false
-				}
-			}, NewSkipObject(tc.slice1, "tc.slice1"), NewSkipObject(tc.map1, "tc.map1"))
-			assert.Equal(t, tc.skippedAll, result)
-
-			SkipIfEmptyAny(func(s string, i ...int) {
-				if strings.Contains(s, "Test skipped") {
-					result = true
-				} else {
-					result = false
-				}
-			}, NewSkipObject(tc.slice1, "tc.slice1"), NewSkipObject(tc.map1, "tc.map1"))
-			assert.Equal(t, tc.skippedAny, result)
-		}
 	}
 }
