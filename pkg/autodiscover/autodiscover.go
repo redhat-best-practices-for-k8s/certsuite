shimritproj/cnf-certification-test

Updated resources
Reference to the scenario where no labels are assigned to any resources

pkg/autodiscover/autodiscover.go
@@ -153,11 +153,11 @@ func DoAutoDiscover(config *configuration.TestConfiguration) DiscoveredTestData 
 	data.AllInstallPlans = getAllInstallPlans(oc.OlmClient)
 	data.AllCatalogSources = getAllCatalogSources(oc.OlmClient)
 	data.Namespaces = namespacesListToStringList(config.TargetNameSpaces)
-	data.Pods, data.AllPods = findPodsByLabel(oc.K8sClient.CoreV1(), podsUnderTestLabelsObjects, data.Namespaces)
+	data.Pods, data.AllPods = findPodsUnderTest(oc.K8sClient.CoreV1(), podsUnderTestLabelsObjects, data.Namespaces)
 	data.AbnormalEvents = findAbnormalEvents(oc.K8sClient.CoreV1(), data.Namespaces)
 	debugLabels := []labelObject{{LabelKey: debugHelperPodsLabelName, LabelValue: debugHelperPodsLabelValue}}
 	debugNS := []string{config.DebugDaemonSetNamespace}
-	data.DebugPods, _ = findPodsByLabel(oc.K8sClient.CoreV1(), debugLabels, debugNS)
+	data.DebugPods, _ = findPodsUnderTest(oc.K8sClient.CoreV1(), debugLabels, debugNS)
 	data.ResourceQuotaItems, err = getResourceQuotas(oc.K8sClient.CoreV1())
 	if err != nil {
 		log.Error("Cannot get resource quotas, err: %v", err)
@@ -175,7 +175,7 @@ func DoAutoDiscover(config *configuration.TestConfiguration) DiscoveredTestData 
 	}
 	data.Crds = FindTestCrdNames(config.CrdFilters)
 	data.ScaleCrUnderTest = GetScaleCrUnderTest(data.Namespaces, data.Crds)
-	data.Csvs = findOperatorsByLabel(oc.OlmClient, operatorsUnderTestLabelsObjects, config.TargetNameSpaces)
+	data.Csvs = findOperatorsUnderTest(oc.OlmClient, operatorsUnderTestLabelsObjects, config.TargetNameSpaces)
 	data.Subscriptions = findSubscriptions(oc.OlmClient, data.Namespaces)
 	data.HelmChartReleases = getHelmList(oc.RestConfig, data.Namespaces)
 
@@ -199,8 +199,8 @@ func DoAutoDiscover(config *configuration.TestConfiguration) DiscoveredTestData 
 	data.OCPStatus = compatibility.DetermineOCPStatus(openshiftVersion, time.Now())
 
 	data.K8sVersion = k8sVersion.GitVersion
-	data.Deployments = findDeploymentByLabel(oc.K8sClient.AppsV1(), podsUnderTestLabelsObjects, data.Namespaces)
-	data.StatefulSet = findStatefulSetByLabel(oc.K8sClient.AppsV1(), podsUnderTestLabelsObjects, data.Namespaces)
+	data.Deployments = findDeploymentsUnderTest(oc.K8sClient.AppsV1(), podsUnderTestLabelsObjects, data.Namespaces)
+	data.StatefulSet = findStatefulSetsUnderTest(oc.K8sClient.AppsV1(), podsUnderTestLabelsObjects, data.Namespaces)
 	// Find ClusterRoleBindings
 	clusterRoleBindings, err := getClusterRoleBindings(oc.K8sClient.RbacV1())
 	if err != nil {

pkg/autodiscover/autodiscover_operators.go
@@ -63,32 +63,47 @@ func isIstioServiceMeshInstalled(allNs []string) bool {
 	return true
 }
 
-func findOperatorsByLabel(olmClient clientOlm.Interface, labels []labelObject, namespaces []configuration.Namespace) []*olmv1Alpha.ClusterServiceVersion {
-	csvs := []*olmv1Alpha.ClusterServiceVersion{}
+func findOperatorsMatchingAtLeastOneLabel(olmClient clientOlm.Interface, labels []labelObject, namespace configuration.Namespace) *olmv1Alpha.ClusterServiceVersionList {
+	csvList := &olmv1Alpha.ClusterServiceVersionList{}
+	for _, l := range labels {
+		log.Debug("Searching CSVs in namespace %q with label %q", namespace, l)
+		csv, err := olmClient.OperatorsV1alpha1().ClusterServiceVersions(namespace.Name).List(context.TODO(), metav1.ListOptions{
+			LabelSelector: l.LabelKey + "=" + l.LabelValue,
+		})
+		if err != nil {
+			log.Error("Error when listing csvs in namespace %q with label %q, err: %v", namespace, l.LabelKey+"="+l.LabelValue, err)
+			continue
+		}
+		csvList.Items = append(csvList.Items, csv.Items...)
+	}
+	return csvList
+}
+func findOperatorsUnderTest(olmClient clientOlm.Interface, labels []labelObject, namespaces []configuration.Namespace) (csvs []*olmv1Alpha.ClusterServiceVersion) {
+	csvs = []*olmv1Alpha.ClusterServiceVersion{}
+	var csvList *olmv1Alpha.ClusterServiceVersionList
+	var err error
 	for _, ns := range namespaces {
-		for _, aLabelObject := range labels {
-			label := aLabelObject.LabelKey + "=" + aLabelObject.LabelValue
-			log.Debug("Searching CSVs in namespace %q with label %q", ns, label)
-			csvList, err := olmClient.OperatorsV1alpha1().ClusterServiceVersions(ns.Name).List(context.TODO(), metav1.ListOptions{
-				LabelSelector: label,
-			})
-			if err != nil {
-				log.Error("Error when listing csvs in namespace %q with label %q", ns, label)
-				continue
-			}
-
-			for i := range csvList.Items {
-				csvs = append(csvs, &csvList.Items[i])
-			}
+		if len(labels) > 0 {
+			csvList = findOperatorsMatchingAtLeastOneLabel(olmClient, labels, ns)
+		} else {
+			// If labels are not provided in the namespace under test, they are tested by the CNF suite
+			log.Debug("Searching CSVs in namespace %s without label", ns)
+			csvList, err = olmClient.OperatorsV1alpha1().ClusterServiceVersions(ns.Name).List(context.TODO(), metav1.ListOptions{})
+		}
+		if err != nil {
+			log.Error("Error when listing csvs in namespace %q , err: %v", ns, err)
+			continue
+		}
+		for i := range csvList.Items {
+			csvs = append(csvs, &csvList.Items[i])
 		}
 	}
-
 	for i := range csvs {
 		log.Info("Found CSV %q (namespace %q)", csvs[i].Name, csvs[i].Namespace)
 	}
-
 	return csvs
 }
+
 func getAllNamespaces(oc corev1client.CoreV1Interface) (allNs []string, err error) {
 	nsList, err := oc.Namespaces().List(context.TODO(), metav1.ListOptions{})
 	if err != nil {

pkg/autodiscover/autodiscover_pods.go
@@ -25,30 +25,48 @@ import (
 	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
 )
 
-func findPodsByLabel(oc corev1client.CoreV1Interface, labels []labelObject, namespaces []string) (runningPods, allPods []corev1.Pod) {
+func findPodsMatchingAtLeastOneLabel(oc corev1client.CoreV1Interface, labels []labelObject, namespace string) *corev1.PodList {
+	allPods := &corev1.PodList{}
+	for _, l := range labels {
+		log.Debug("Searching Pods in namespace %s with label %q", namespace, l)
+		pods, err := oc.Pods(namespace).List(context.TODO(), metav1.ListOptions{
+			LabelSelector: l.LabelKey + "=" + l.LabelValue,
+		})
+		if err != nil {
+			log.Error("Error when listing pods in ns=%s label=%s, err: %v", namespace, l.LabelKey+"="+l.LabelValue, err)
+			continue
+		}
+		allPods.Items = append(allPods.Items, pods.Items...)
+	}
+	return allPods
+}
+func findPodsUnderTest(oc corev1client.CoreV1Interface, labels []labelObject, namespaces []string) (runningPods, allPods []corev1.Pod) {
 	runningPods = []corev1.Pod{}
 	allPods = []corev1.Pod{}
+	// Iterate through namespaces
 	for _, ns := range namespaces {
-		for _, aLabelObject := range labels {
-			label := aLabelObject.LabelKey + "=" + aLabelObject.LabelValue
-			log.Debug("Searching Pods with label %q", label)
-			pods, err := oc.Pods(ns).List(context.TODO(), metav1.ListOptions{
-				LabelSelector: label,
-			})
-			if err != nil {
-				log.Error("Error when listing pods in ns=%s label=%s, err: %v", ns, label, err)
-				continue
-			}
+		var pods *corev1.PodList
+		var err error
+		if len(labels) > 0 {
+			pods = findPodsMatchingAtLeastOneLabel(oc, labels, ns)
+		} else {
+			// If labels are not provided in the namespace under test, they are tested by the CNF suite
+			log.Debug("Searching Pods in namespace %s without label", ns)
+			pods, err = oc.Pods(ns).List(context.TODO(), metav1.ListOptions{})
+		}
 
-			// Filter out any pod set to be deleted
-			for i := 0; i < len(pods.Items); i++ {
-				if pods.Items[i].ObjectMeta.DeletionTimestamp == nil &&
-					pods.Items[i].Status.Phase == corev1.PodRunning {
-					runningPods = append(runningPods, pods.Items[i])
-				}
-				allPods = append(allPods, pods.Items[i])
+		if err != nil {
+			log.Error("Error when listing pods in ns=%s, err: %v", ns, err)
+			continue
+		}
+		// Filter out any pod set to be deleted
+		for i := 0; i < len(pods.Items); i++ {
+			if pods.Items[i].ObjectMeta.DeletionTimestamp == nil && pods.Items[i].Status.Phase == corev1.PodRunning {
+				runningPods = append(runningPods, pods.Items[i])
 			}
+			allPods = append(allPods, pods.Items[i])
 		}
 	}
+
 	return runningPods, allPods
 }

pkg/autodiscover/autodiscover_pods_test.go
@@ -27,7 +27,7 @@ import (
 	"github.com/test-network-function/cnf-certification-test/internal/clientsholder"
 )
 
-func TestFindPodsByLabel(t *testing.T) {
+func TestFindPodsUnderTest(t *testing.T) {
 	generatePod := func(podname, namespace, label string) *corev1.Pod {
 		return &corev1.Pod{
 			ObjectMeta: metav1.ObjectMeta{
@@ -91,7 +91,7 @@ func TestFindPodsByLabel(t *testing.T) {
 		testRuntimeObjects = append(testRuntimeObjects, generatePod(tc.testPodName, tc.testPodNamespace, tc.queryLabel))
 		oc := clientsholder.GetTestClientsHolder(testRuntimeObjects)
 
-		podResult, _ := findPodsByLabel(oc.K8sClient.CoreV1(), testLabel, testNamespaces)
+		podResult, _ := findPodsUnderTest(oc.K8sClient.CoreV1(), testLabel, testNamespaces)
 		assert.Equal(t, tc.expectedResults, podResult)
 	}
 }

pkg/autodiscover/autodiscover_podset_test.go
@@ -28,7 +28,7 @@ import (
 	"github.com/test-network-function/cnf-certification-test/internal/clientsholder"
 )
 
-func TestFindDeploymentByLabel(t *testing.T) {
+func TestFindDeploymentsUnderTest(t *testing.T) {
 	generateDeployment := func(name, namespace, label string) *appsv1.Deployment {
 		return &appsv1.Deployment{
 			ObjectMeta: metav1.ObjectMeta{
@@ -99,12 +99,12 @@ func TestFindDeploymentByLabel(t *testing.T) {
 		testRuntimeObjects = append(testRuntimeObjects, generateDeployment(tc.testDeploymentName, tc.testDeploymentNamespace, tc.queryLabel))
 		oc := clientsholder.GetTestClientsHolder(testRuntimeObjects)
 
-		deployments := findDeploymentByLabel(oc.K8sClient.AppsV1(), testLabel, testNamespaces)
+		deployments := findDeploymentsUnderTest(oc.K8sClient.AppsV1(), testLabel, testNamespaces)
 		assert.Equal(t, tc.expectedResults, deployments)
 	}
 }
 
-func TestFindStatefulSetByLabel(t *testing.T) {
+func TestFindStatefulSetsUnderTest(t *testing.T) {
 	generateStatefulSet := func(name, namespace, label string) *appsv1.StatefulSet {
 		return &appsv1.StatefulSet{
 			ObjectMeta: metav1.ObjectMeta{
@@ -174,7 +174,7 @@ func TestFindStatefulSetByLabel(t *testing.T) {
 		testRuntimeObjects = append(testRuntimeObjects, generateStatefulSet(tc.testStatefulSetName, tc.testStatefulSetNamespace, tc.queryLabel))
 		oc := clientsholder.GetTestClientsHolder(testRuntimeObjects)
 
-		statefulSets := findStatefulSetByLabel(oc.K8sClient.AppsV1(), testLabel, testNamespaces)
+		statefulSets := findStatefulSetsUnderTest(oc.K8sClient.AppsV1(), testLabel, testNamespaces)
 		assert.Equal(t, tc.expectedResults, statefulSets)
 	}
 }

redhat-best-practices-for-k8s/certsuite

pkg/autodiscover/autodiscover.go
@@ -154,11 +154,11 @@ func DoAutoDiscover(config *configuration.TestConfiguration) DiscoveredTestData 
 	data.AllInstallPlans = getAllInstallPlans(oc.OlmClient)
 	data.AllCatalogSources = getAllCatalogSources(oc.OlmClient)
 	data.Namespaces = namespacesListToStringList(config.TargetNameSpaces)
-	data.Pods, data.AllPods = findPodsByLabel(oc.K8sClient.CoreV1(), podsUnderTestLabelsObjects, data.Namespaces)
+	data.Pods, data.AllPods = findPodsByLabels(oc.K8sClient.CoreV1(), podsUnderTestLabelsObjects, data.Namespaces)
 	data.AbnormalEvents = findAbnormalEvents(oc.K8sClient.CoreV1(), data.Namespaces)
 	debugLabels := []labelObject{{LabelKey: debugHelperPodsLabelName, LabelValue: debugHelperPodsLabelValue}}
 	debugNS := []string{config.DebugDaemonSetNamespace}
-	data.DebugPods, _ = findPodsByLabel(oc.K8sClient.CoreV1(), debugLabels, debugNS)
+	data.DebugPods, _ = findPodsByLabels(oc.K8sClient.CoreV1(), debugLabels, debugNS)
 	data.ResourceQuotaItems, err = getResourceQuotas(oc.K8sClient.CoreV1())
 	if err != nil {
 		log.Error("Cannot get resource quotas, err: %v", err)
@@ -176,7 +176,7 @@ func DoAutoDiscover(config *configuration.TestConfiguration) DiscoveredTestData 
 	}
 	data.Crds = FindTestCrdNames(config.CrdFilters)
 	data.ScaleCrUnderTest = GetScaleCrUnderTest(data.Namespaces, data.Crds)
-	data.Csvs = findOperatorsByLabel(oc.OlmClient, operatorsUnderTestLabelsObjects, config.TargetNameSpaces)
+	data.Csvs = findOperatorsByLabels(oc.OlmClient, operatorsUnderTestLabelsObjects, config.TargetNameSpaces)
 	data.Subscriptions = findSubscriptions(oc.OlmClient, data.Namespaces)
 	data.HelmChartReleases = getHelmList(oc.RestConfig, data.Namespaces)
 
@@ -199,8 +199,8 @@ func DoAutoDiscover(config *configuration.TestConfiguration) DiscoveredTestData 
 	data.OCPStatus = compatibility.DetermineOCPStatus(openshiftVersion, time.Now())
 
 	data.K8sVersion = k8sVersion.GitVersion
-	data.Deployments = findDeploymentByLabel(oc.K8sClient.AppsV1(), podsUnderTestLabelsObjects, data.Namespaces)
-	data.StatefulSet = findStatefulSetByLabel(oc.K8sClient.AppsV1(), podsUnderTestLabelsObjects, data.Namespaces)
+	data.Deployments = findDeploymentsByLabels(oc.K8sClient.AppsV1(), podsUnderTestLabelsObjects, data.Namespaces)
+	data.StatefulSet = findStatefulSetsByLabels(oc.K8sClient.AppsV1(), podsUnderTestLabelsObjects, data.Namespaces)
 
 	// Check if the Istio Service Mesh is present
 	data.IstioServiceMeshFound = isIstioServiceMeshInstalled(oc.K8sClient.AppsV1(), data.AllNamespaces)

pkg/autodiscover/autodiscover_operators.go
@@ -61,32 +61,48 @@ func isIstioServiceMeshInstalled(appClient appv1client.AppsV1Interface, allNs []
 	return true
 }
 
-func findOperatorsByLabel(olmClient clientOlm.Interface, labels []labelObject, namespaces []configuration.Namespace) []*olmv1Alpha.ClusterServiceVersion {
-	csvs := []*olmv1Alpha.ClusterServiceVersion{}
+func findOperatorsMatchingAtLeastOneLabel(olmClient clientOlm.Interface, labels []labelObject, namespace configuration.Namespace) *olmv1Alpha.ClusterServiceVersionList {
+	csvList := &olmv1Alpha.ClusterServiceVersionList{}
+	for _, l := range labels {
+		log.Debug("Searching CSVs in namespace %q with label %q", namespace, l)
+		csv, err := olmClient.OperatorsV1alpha1().ClusterServiceVersions(namespace.Name).List(context.TODO(), metav1.ListOptions{
+			LabelSelector: l.LabelKey + "=" + l.LabelValue,
+		})
+		if err != nil {
+			log.Error("Error when listing csvs in namespace %q with label %q, err: %v", namespace, l.LabelKey+"="+l.LabelValue, err)
+			continue
+		}
+		csvList.Items = append(csvList.Items, csv.Items...)
+	}
+	return csvList
+}
+
+func findOperatorsByLabels(olmClient clientOlm.Interface, labels []labelObject, namespaces []configuration.Namespace) (csvs []*olmv1Alpha.ClusterServiceVersion) {
+	csvs = []*olmv1Alpha.ClusterServiceVersion{}
+	var csvList *olmv1Alpha.ClusterServiceVersionList
 	for _, ns := range namespaces {
-		for _, aLabelObject := range labels {
-			label := aLabelObject.LabelKey + "=" + aLabelObject.LabelValue
-			log.Debug("Searching CSVs in namespace %q with label %q", ns, label)
-			csvList, err := olmClient.OperatorsV1alpha1().ClusterServiceVersions(ns.Name).List(context.TODO(), metav1.ListOptions{
-				LabelSelector: label,
-			})
+		if len(labels) > 0 {
+			csvList = findOperatorsMatchingAtLeastOneLabel(olmClient, labels, ns)
+		} else {
+			// If labels are not provided in the namespace under test, they are tested by the CNF suite
+			log.Debug("Searching CSVs in namespace %s without label", ns)
+			var err error
+			csvList, err = olmClient.OperatorsV1alpha1().ClusterServiceVersions(ns.Name).List(context.TODO(), metav1.ListOptions{})
 			if err != nil {
-				log.Error("Error when listing csvs in namespace %q with label %q", ns, label)
+				log.Error("Error when listing csvs in namespace %q , err: %v", ns, err)
 				continue
 			}
-
-			for i := range csvList.Items {
-				csvs = append(csvs, &csvList.Items[i])
-			}
+		}
+		for i := range csvList.Items {
+			csvs = append(csvs, &csvList.Items[i])
 		}
 	}
-
 	for i := range csvs {
 		log.Info("Found CSV %q (namespace %q)", csvs[i].Name, csvs[i].Namespace)
 	}
-
 	return csvs
 }
+
 func getAllNamespaces(oc corev1client.CoreV1Interface) (allNs []string, err error) {
 	nsList, err := oc.Namespaces().List(context.TODO(), metav1.ListOptions{})
 	if err != nil {

pkg/autodiscover/autodiscover_pods.go
@@ -25,30 +25,48 @@ import (
 	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
 )
 
-func findPodsByLabel(oc corev1client.CoreV1Interface, labels []labelObject, namespaces []string) (runningPods, allPods []corev1.Pod) {
+func findPodsMatchingAtLeastOneLabel(oc corev1client.CoreV1Interface, labels []labelObject, namespace string) *corev1.PodList {
+	allPods := &corev1.PodList{}
+	for _, l := range labels {
+		log.Debug("Searching Pods in namespace %s with label %q", namespace, l)
+		pods, err := oc.Pods(namespace).List(context.TODO(), metav1.ListOptions{
+			LabelSelector: l.LabelKey + "=" + l.LabelValue,
+		})
+		if err != nil {
+			log.Error("Error when listing pods in ns=%s label=%s, err: %v", namespace, l.LabelKey+"="+l.LabelValue, err)
+			continue
+		}
+		allPods.Items = append(allPods.Items, pods.Items...)
+	}
+	return allPods
+}
+
+func findPodsByLabels(oc corev1client.CoreV1Interface, labels []labelObject, namespaces []string) (runningPods, allPods []corev1.Pod) {
 	runningPods = []corev1.Pod{}
 	allPods = []corev1.Pod{}
+	// Iterate through namespaces
 	for _, ns := range namespaces {
-		for _, aLabelObject := range labels {
-			label := aLabelObject.LabelKey + "=" + aLabelObject.LabelValue
-			log.Debug("Searching Pods with label %q", label)
-			pods, err := oc.Pods(ns).List(context.TODO(), metav1.ListOptions{
-				LabelSelector: label,
-			})
+		var pods *corev1.PodList
+		if len(labels) > 0 {
+			pods = findPodsMatchingAtLeastOneLabel(oc, labels, ns)
+		} else {
+			// If labels are not provided in the namespace under test, they are tested by the CNF suite
+			log.Debug("Searching Pods in namespace %s without label", ns)
+			var err error
+			pods, err = oc.Pods(ns).List(context.TODO(), metav1.ListOptions{})
 			if err != nil {
-				log.Error("Error when listing pods in ns=%s label=%s, err: %v", ns, label, err)
+				log.Error("Error when listing pods in ns=%s, err: %v", ns, err)
 				continue
 			}
-
-			// Filter out any pod set to be deleted
-			for i := 0; i < len(pods.Items); i++ {
-				if pods.Items[i].ObjectMeta.DeletionTimestamp == nil &&
-					pods.Items[i].Status.Phase == corev1.PodRunning {
-					runningPods = append(runningPods, pods.Items[i])
-				}
-				allPods = append(allPods, pods.Items[i])
+		}
+		// Filter out any pod set to be deleted
+		for i := 0; i < len(pods.Items); i++ {
+			if pods.Items[i].ObjectMeta.DeletionTimestamp == nil && pods.Items[i].Status.Phase == corev1.PodRunning {
+				runningPods = append(runningPods, pods.Items[i])
 			}
+			allPods = append(allPods, pods.Items[i])
 		}
 	}
+
 	return runningPods, allPods
 }

pkg/autodiscover/autodiscover_pods_test.go
@@ -27,7 +27,7 @@ import (
 	"github.com/test-network-function/cnf-certification-test/internal/clientsholder"
 )
 
-func TestFindPodsByLabel(t *testing.T) {
+func TestFindPodsUnderTest(t *testing.T) {
 	generatePod := func(podname, namespace, label string) *corev1.Pod {
 		return &corev1.Pod{
 			ObjectMeta: metav1.ObjectMeta{
@@ -91,7 +91,7 @@ func TestFindPodsByLabel(t *testing.T) {
 		testRuntimeObjects = append(testRuntimeObjects, generatePod(tc.testPodName, tc.testPodNamespace, tc.queryLabel))
 		oc := clientsholder.GetTestClientsHolder(testRuntimeObjects)
 
-		podResult, _ := findPodsByLabel(oc.K8sClient.CoreV1(), testLabel, testNamespaces)
+		podResult, _ := findPodsByLabels(oc.K8sClient.CoreV1(), testLabel, testNamespaces)
 		assert.Equal(t, tc.expectedResults, podResult)
 	}
 }

pkg/autodiscover/autodiscover_podset_test.go
@@ -28,7 +28,7 @@ import (
 	"github.com/test-network-function/cnf-certification-test/internal/clientsholder"
 )
 
-func TestFindDeploymentByLabel(t *testing.T) {
+func TestFindDeploymentsUnderTest(t *testing.T) {
 	generateDeployment := func(name, namespace, label string) *appsv1.Deployment {
 		return &appsv1.Deployment{
 			ObjectMeta: metav1.ObjectMeta{
@@ -99,12 +99,12 @@ func TestFindDeploymentByLabel(t *testing.T) {
 		testRuntimeObjects = append(testRuntimeObjects, generateDeployment(tc.testDeploymentName, tc.testDeploymentNamespace, tc.queryLabel))
 		oc := clientsholder.GetTestClientsHolder(testRuntimeObjects)
 
-		deployments := findDeploymentByLabel(oc.K8sClient.AppsV1(), testLabel, testNamespaces)
+		deployments := findDeploymentsByLabels(oc.K8sClient.AppsV1(), testLabel, testNamespaces)
 		assert.Equal(t, tc.expectedResults, deployments)
 	}
 }
 
-func TestFindStatefulSetByLabel(t *testing.T) {
+func TestFindStatefulSetsUnderTest(t *testing.T) {
 	generateStatefulSet := func(name, namespace, label string) *appsv1.StatefulSet {
 		return &appsv1.StatefulSet{
 			ObjectMeta: metav1.ObjectMeta{
@@ -174,7 +174,7 @@ func TestFindStatefulSetByLabel(t *testing.T) {
 		testRuntimeObjects = append(testRuntimeObjects, generateStatefulSet(tc.testStatefulSetName, tc.testStatefulSetNamespace, tc.queryLabel))
 		oc := clientsholder.GetTestClientsHolder(testRuntimeObjects)
 
-		statefulSets := findStatefulSetByLabel(oc.K8sClient.AppsV1(), testLabel, testNamespaces)
+		statefulSets := findStatefulSetsByLabels(oc.K8sClient.AppsV1(), testLabel, testNamespaces)
 		assert.Equal(t, tc.expectedResults, statefulSets)
 	}
 }