96e25ff1e0a59853ebbba9702ae63f60a3cf6723
Gonzalo Reyero Ferreras <greyerof@redhat.com>
2024-08-16 06:22:46 -0500

    Fix for the false positive of access-control's capabilities tcs.
    The forbidden capabilities test cases in access-control test suite are
    giving false positives. The code was stringifying the whole
    "capabilities" field and searching for the capability in the resulting
    string, which led to false positives when the capability was set in the
    "drop" list, as we've seen in a partner.
    
    The new code checks whether it was explicitly set in the Add list
    directly, matching by capability name or by "ALL".

--- tests/accesscontrol/suite_test.go
+++ tests/accesscontrol/suite_test.go
@@ -15,3 +15,112 @@
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 package accesscontrol
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
+)
+
+func Test_isContainerCapabilitySet(t *testing.T) {
+	type args struct {
+		containerCapabilities *v1.Capabilities
+		capability            string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "nil capabilities",
+			args: args{
+				capability:            "IPC_LOCK",
+				containerCapabilities: nil,
+			},
+			want: false,
+		},
+		{
+			name: "empty capabilities",
+			args: args{
+				capability:            "IPC_LOCK",
+				containerCapabilities: &v1.Capabilities{},
+			},
+			want: false,
+		},
+		{
+			name: "explicitly empty add list",
+			args: args{
+				capability:            "IPC_LOCK",
+				containerCapabilities: &v1.Capabilities{Add: []v1.Capability{}},
+			},
+			want: false,
+		},
+		{
+			name: "explicitly empty drop list",
+			args: args{
+				capability:            "IPC_LOCK",
+				containerCapabilities: &v1.Capabilities{Drop: []v1.Capability{}},
+			},
+			want: false,
+		},
+		{
+			name: "IPC_LOCK not found in any list",
+			args: args{
+				capability: "IPC_LOCK",
+				containerCapabilities: &v1.Capabilities{
+					Add:  []v1.Capability{"NET_CAP_BINDING"},
+					Drop: []v1.Capability{"SYS_ADMIN", "NET_ADMIN"},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "IPC_LOCK found in the add list",
+			args: args{
+				capability: "IPC_LOCK",
+				containerCapabilities: &v1.Capabilities{
+					Add:  []v1.Capability{"NET_ADMIN", "IPC_LOCK"},
+					Drop: []v1.Capability{"SYS_ADMIN"}},
+			},
+			want: true,
+		},
+		{
+			name: "IPC_LOCK appears in the drop list only",
+			args: args{
+				capability:            "IPC_LOCK",
+				containerCapabilities: &v1.Capabilities{Drop: []v1.Capability{"SYS_ADMIN", "IPC_LOCK", "NET_ADMIN"}},
+			},
+			want: false,
+		},
+		{
+			// When set in both add and drop lists, k8s/openshift will compute drop first, then add, which results
+			// in the capability to be finally set.
+			name: "IPC_LOCK set in both add and drop lists.",
+			args: args{
+				capability: "IPC_LOCK",
+				containerCapabilities: &v1.Capabilities{
+					Add:  []v1.Capability{"IPC_LOCK"},
+					Drop: []v1.Capability{"SYS_ADMIN", "IPC_LOCK", "NET_ADMIN"},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "ALL capabilities in the add list",
+			args: args{
+				capability:            "IPC_LOCK",
+				containerCapabilities: &v1.Capabilities{Add: []v1.Capability{"ALL"}},
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isContainerCapabilitySet(tt.args.containerCapabilities, tt.args.capability); got != tt.want {
+				assert.Equal(t, tt.want, got)
+			}
+		})
+	}
+}
