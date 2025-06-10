5c4115bb48a796c1240972d99c62a575d69ae737
Brandon Palm <bpalm@redhat.com>
2025-06-10 11:19:04 -0500

    script: update rekick script with 4.18 jobs (#3012)

--- script/rekick-failed-workflows.sh
+++ script/rekick-failed-workflows.sh
@@ -15,10 +15,12 @@ WORKFLOWS_TO_CHECK=(
 	"QE OCP 4.15 Testing"
 	"QE OCP 4.16 Testing"
 	"QE OCP 4.17 Testing"
+	"QE OCP 4.18 Testing"
 	"QE OCP 4.14 Intrusive Testing"
 	"QE OCP 4.15 Intrusive Testing"
 	"QE OCP 4.16 Intrusive Testing"
 	"QE OCP 4.17 Intrusive Testing"
+	"QE OCP 4.18 Intrusive Testing"
 	"OCP ARM64 4.16 QE Testing"
 	"qe-ocp-hosted.yml"
 )
