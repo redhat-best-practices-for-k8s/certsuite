6b94519f44fa527db49a1e9a09242936a59c1f1d
Brandon Palm <bpalm@redhat.com>
2025-11-18 08:54:39 -0600

    Add 4.20 workflows to rekick script

--- script/rekick-failed-workflows.sh
+++ script/rekick-failed-workflows.sh
@@ -57,11 +57,13 @@ WORKFLOWS_TO_CHECK=(
 	"QE OCP 4.17 Testing"
 	"QE OCP 4.18 Testing"
 	"QE OCP 4.19 Testing"
+	"QE OCP 4.20 Testing"
 	"QE OCP 4.14 Intrusive Testing"
 	"QE OCP 4.16 Intrusive Testing"
 	"QE OCP 4.17 Intrusive Testing"
 	"QE OCP 4.18 Intrusive Testing"
 	"QE OCP 4.19 Intrusive Testing"
+	"QE OCP 4.20 Intrusive Testing"
 	"OCP ARM64 4.16 QE Testing"
 	"qe-ocp-hosted.yml"
 )
